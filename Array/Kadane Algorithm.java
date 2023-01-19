69b9ddd5fdac71f7ce0ddbed9bfadf943cd6d0bf
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-01-20 01:50:30 +0530

    Delete Kadane Algorithm.java

--- Array/Kadane Algorithm.java
+++
@@ -1,9 +0,0 @@
-public static int kadaneAlgo(int nums,int n){
-    int sum = nums[0];
-    int maxSum= nums[0];
-    for(int i=1; i<n; i++)
-    {
-        sum = Math.max(sum + nums[i], nums[i]);
-        maxSum= Math.max(sum, maxSum);
-    }
-}
