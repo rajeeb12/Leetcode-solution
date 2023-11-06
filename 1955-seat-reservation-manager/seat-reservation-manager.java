ebf12da2de99142967d72ab1ff1305ad0f54288c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-11-06 13:25:02 +0530

    Time: 29 ms (94.06%) | Memory: 91.6 MB (17.62%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,21 +1,73 @@
+//optimal
 class SeatManager {
     PriorityQueue<Integer> pq;
+    int seatMarker;
     public SeatManager(int n) {
         pq = new PriorityQueue<>((a,b) -> a - b);
-        for(int i= 1 ; i <= n ; i++){
-            pq.add(i);
-        }
+        seatMarker = 1;
     }
     
     public int reserve() {
-
-        return pq.poll();
+        if(!pq.isEmpty()){
+            int seat = pq.peek();
+            pq.poll();
+            return seat;
+        }
+        int currentSeat = seatMarker;
+        seatMarker++;
+        return currentSeat;
     }
     
     public void unreserve(int seatNumber) {
         pq.add(seatNumber);
     }
 }
+
+//Brute 
+// class SeatManager {
+//     int seats[];
+//     public SeatManager(int n) {
+//         seats = new int[n + 1];
+//         Arrays.fill(seats, -1);
+//     }
+    
+//     public int reserve() {
+//        int reserve = -1;
+//        for(int i = 1 ; i < seats.length ; i++){
+//            if(seats[i] == -1){
+//                seats[i] = 1;
+//                reserve = i;
+//                break;
+//            }
+//        }
+//        return reserve;
+//     }
+    
+//     public void unreserve(int seatNumber) {
+//         seats[seatNumber] = -1;
+//     }
+// }
+
+//Better
+// class SeatManager {
+//     PriorityQueue<Integer> pq;
+//     public SeatManager(int n) {
+//         pq = new PriorityQueue<>((a,b) -> a - b);
+//         for(int i= 1 ; i <= n ; i++){
+//             pq.add(i);
+//         }
+//     }
+    
+//     public int reserve() {
+
+//         return pq.poll();
+//     }
+    
+//     public void unreserve(int seatNumber) {
+//         pq.add(seatNumber);
+//     }
+// }
+
 
 /**
  * Your SeatManager object will be instantiated and called as such:
