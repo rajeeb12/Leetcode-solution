abcf6ccd244fc38d365ce50576d9d8edb3c50306
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-11-16 11:32:03 +0530

    Time: 233 ms (12.91%) | Memory: 54.9 MB (5.64%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -2,22 +2,32 @@ class Solution {
     public String findDifferentBinaryString(String[] nums) {
         if(nums.length == 0) return "";
         ArrayList<String> list = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
+        for(String str : nums){
+            set.add(str);
+        } 
         int n = nums.length;
         generateStrings(n,"",list);
-        String result ="";
-        for(int i = 0 ; i < list.size() ; i++){
-            String str = list.get(i);
-            boolean exist = false;
-            for(int j = 0 ; j < n ; j++){
-                if(str.equals(nums[j])){
-                    exist = true;
-                }
-            }
-            if(!exist){
-                result = str;
+        for(String str : list){
+            if(!set.contains(str)){
+                return str;
             }
         }
-        return result;
+
+        // String result ="";
+        // for(int i = 0 ; i < list.size() ; i++){
+        //     String str = list.get(i);
+        //     boolean exist = false;
+        //     for(int j = 0 ; j < n ; j++){
+        //         if(str.equals(nums[j])){
+        //             exist = true;
+        //         }
+        //     }
+        //     if(!exist){
+        //         result = str;
+        //     }
+        // }
+        return "";
     }
     public void generateStrings(int n,String str,ArrayList<String> list){
         if(n == str.length()){
