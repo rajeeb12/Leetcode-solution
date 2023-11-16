d264bc7ac206e16a17ea3032762720235c2700d7
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-11-16 12:17:30 +0530

    Time: 1 ms (77.09%) | Memory: 40.7 MB (41.82%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,23 +1,13 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
-        for(String str : nums){
-            set.add(Integer.parseInt(str, 2)); 
-            // convert string  -> binary int , int -> decimal 
-        }
-
-        String result= "";
-        for(int i = 0 ; i <= n; i++){
-            if(!set.contains(i)){
-                result = Integer.toBinaryString(i); //convert the int -> binary int, binary int -> string
-
-                while(result.length() < n){
-                    result = '0' + result;
-                }
-                return result;
+        String result ="";
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i].charAt(i) == '1'){
+                result += '0'; 
+            }else{
+                result += '1';
             }
         }
-        return "";
+        return result;
     }
 }
