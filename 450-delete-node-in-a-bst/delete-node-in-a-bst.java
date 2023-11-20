eacf6c2dfb0b476b768932c197dd1a50ccdc13a9
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-11-20 19:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (26.41%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode dummy = root;
+
+        if(root == null) return root;
+        if(root.val == key){
+            return helper(root);
+        }
+        while(root != null){
+            if(root.val < key){
+                if(root.right != null  && root.right.val == key){
+                    root.right = helper(root.right); 
+                    break;
+                }else{
+                    root = root.right;
+                }
+            }else{
+                if(root.left != null  && root.left.val == key){
+                    root.left = helper(root.left);
+                    break; 
+                }else{
+                    root = root.left;
+                }
+            }
+        }
+        return dummy;
+    }
+    public TreeNode helper(TreeNode root){
+        if(root.left == null) return root.right;
+        else if(root.right == null) return root.left;
+
+        TreeNode rightChild = root.right;
+        TreeNode leftRightMostChild = find(root.left); 
+        leftRightMostChild.right = rightChild;
+        return root.left;
+    }
+    public TreeNode find(TreeNode root){
+        if(root.right == null) return root;
+
+        return find(root.right);
+    } 
+}
