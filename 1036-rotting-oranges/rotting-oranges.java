570734a71956d8b22ab12fd0318b7ce85fd1711b
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-11-23 01:41:58 +0530

    Time: 10 ms (5.74%) | Memory: 42.3 MB (11.99%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q= new LinkedList<>();
+        int n =  grid.length;
+        int m = grid[0].length;
+        int countOranges = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j=0; j < grid[0].length; j++){
+                if(grid[i][j] == 2){ // storing position of 2 in queue
+                    q.add(new int[]{i,j});
+                }
+                if(grid[i][j] != 0){ 
+                    countOranges++;
+                }
+            }
+        } 
+        if(countOranges == 0) return 0;
+
+        int di[]= {-1,1,0,0};
+        int dj[]= {0,0,-1,1};
+        int minTime = 0;
+        int rotten = 0;
+
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            rotten += size;
+            for(int i = 0 ; i < size; i++)
+            {
+                int temp[] = q.poll();
+                int x =temp[0];
+                int y =temp[1];
+                for(int j = 0 ; j < 4; j++)
+                {
+                    int nextX = x + di[j];
+                    int nextY = y + dj[j];
+
+                    if(nextX >=0 && nextY >= 0 && nextX < n && nextY < m && grid[nextX][nextY] != 2 && grid[nextX][nextY] != 0){
+                        grid[nextX][nextY] = 2;
+                        q.add(new int[]{nextX, nextY});
+                    }
+                }
+            }
+            if(!q.isEmpty()) minTime++;
+        }
+        System.out.print(rotten +""+countOranges);
+        return (rotten == countOranges ? minTime : -1);
+
+    }
+}
