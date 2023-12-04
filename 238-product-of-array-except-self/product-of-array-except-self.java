a78c8175e88a9014bee65fa8647fbe1030ff8a0f
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-05 02:23:52 +0530

    Time: 3 ms (13.60%) | Memory: 54 MB (8.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,48 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int p = 1;
-        int ans[] = new int[n];
-        // pehele put then multiply 
-        for(int i = 0 ; i < n ; i++){
-            ans[i] = p;
-            p *= nums[i];
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = nums[0];
+        suffix[n-1] = nums[n-1];
+        for(int i = 1 ; i < n ; i++)
+        {
+            prefix[i] = prefix[i - 1] * nums[i];
+            suffix[n - i - 1] = suffix[n - i] * nums[n - i - 1];  
         }
-        p = 1;
-        // pehele put then multiplu 
-        for(int i = n - 1; i >= 0 ;i--){
-            ans[i] *= p;
-            p *= nums[i];
+        int ans[]= new int[n];
+        ans[0]= suffix[1];
+        ans[n-1] = prefix[n-2];
+        
+        for(int i = 1 ; i < n - 1; i++){
+            ans[i] = prefix[i - 1] * suffix[i + 1];
         }
         return ans;
     }
 }
-// used extra space
-// public int[] productExceptSelf(int[] nums) {
-//         int n = nums.length;
-//         int prefix[]= new int[n];
-//         int suffix[]= new int[n];
-//         int p = 1;
-//         for(int i = 0 ; i < n ; i++){
-//             p *= nums[i];
-//             prefix[i] = p;
-//         }
-//         int s = 1;
-//         for(int i = n-1 ; i >= 0 ; i--){
-//             s *= nums[i];
-//             suffix[i] = s;
-//         }
-//         int ans[]= new int[n];
-//         for(int i = 0 ; i < n ; i++){
-//             if(i <= 0){
-//                 ans[i] = suffix[i + 1];
-//                 continue;
-//             }
-//             if(i == n - 1){
-//                 ans[i] = prefix[i-1];
-//                 continue;
-//             }
-//             ans[i] = prefix[i-1] * suffix[i + 1];
-//         }
-//         return ans;
-//     }
