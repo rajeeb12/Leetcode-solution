02d3e96438b8d03c7b6c473537f4de0956da77ac
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-10 02:02:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (26.25%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -18,25 +18,21 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
 
+        Stack<TreeNode> st = new Stack<>();
         TreeNode cur = root;
-        // morris traversal
-        while(cur!= null){
-            if(cur.left  == null){
-                ans.add(cur.val);
-                cur = cur.right;
-            }else{
-                TreeNode prev = cur.left;
-                while(prev.right != null && prev.right != cur){
-                    prev = prev.right;
-                }
-                if(prev.right == null){
-                    prev.right = cur;
-                    cur = cur.left;
-                }else{
-                    prev.right = null;
-                    ans.add(cur.val);
-                    cur = cur.right;
-                }
+
+        while(true)
+        {
+            if(cur != null)
+            {
+                st.add(cur);
+                cur = cur.left;
+            }
+            else{
+                if(st.isEmpty()) break;
+                TreeNode node = st.pop();
+                ans.add(node.val);
+                cur = node.right;
             }
         }
         return ans;
