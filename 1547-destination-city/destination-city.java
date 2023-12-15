0832b85b86b1f2d76e2476adb06f67a7703984f0
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-15 13:43:47 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (39.87%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,30 +1,14 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        HashMap<String,Integer> map= new HashMap<>();
-        HashMap<Integer,String> map2= new HashMap<>();
-        int index = 0;
-        for(List<String> item:paths)
+        HashSet<String> set = new HashSet<>();
+
+        for(List<String> item: paths)
         {
-            String str1 = item.get(0);
-            String str2 = item.get(1);
-            if(!map.containsKey(str1)){
-                map.put(str1, index);
-                map2.put(index++, str1);
-            }
-            if(!map.containsKey(str2)){
-                map.put(str2, index);
-                map2.put(index++, str2);
-            }
+            set.add(item.get(0));
         }
-        int outDegree[]= new int[index];
-        for(List<String> item:paths)
+        for(List<String> item: paths)
         {
-            int index1 = map.get(item.get(0));
-            outDegree[index1]++;
-        }
-        for(int i = 0 ; i < index; i++)
-        {
-            if(outDegree[i] == 0) return map2.get(i); 
+            if(!set.contains(item.get(1))) return item.get(1);
         }
         return "";
     }
