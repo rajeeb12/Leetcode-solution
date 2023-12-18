685b995cdfaf584854e91612280b428534b3f770
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-19 02:01:32 +0530

    Time: 6 ms (65.58%) | Memory: 43.7 MB (50.50%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,25 +3,18 @@ class Solution {
         if(s.length() == 0 || s.length() == 1) return s.length();
         int left = 0;
         int right = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
         int n = s.length();
         int max = 0;
         while(right < n)
         {
-            char ch= s.charAt(right);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            if(map.get(ch) > 1)
+            if(!set.contains(s.charAt(right)))
             {
-                while(map.get(ch) > 1 && left < right)
-                {
-                    map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
-                    if(map.get(s.charAt(left)) == 0) map.remove(s.charAt(left));
-                    left++;
-                }
+                set.add(s.charAt(right++));
+                max = Math.max(set.size(), max);
+            }else{
+                set.remove(s.charAt(left++));
             }
-            max = Math.max(max, right - left + 1);
-            right++;
         }   
         return max;
     }
