cae4130f36b77ed647ab49002ed238491e790a60
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-20 19:35:28 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (5.53%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,7 +1,19 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int sum = prices[0] + prices[1];
-        return (sum > money ? money : money - sum); 
+        int sm = (int)1e9 , ssm = (int)1e9;
+        // just have to find smallest and second smallest prices.
+
+        for(int i: prices)
+        {
+            if(sm > i)
+            {
+                ssm = sm;
+                sm = i;
+            }else if(ssm > i )
+            {
+                ssm = i;
+            }
+        }
+        return (sm + ssm <= money ? money - (sm + ssm) : money);
     }
 }
