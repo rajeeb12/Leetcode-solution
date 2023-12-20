9fcc0267cc26f1a112a4938a42beb90783e3e21e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-20 21:18:48 +0530

    Time: 13 ms (62.97%) | Memory: 44.6 MB (24.63%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(t.length() > s.length()) return "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0 ;
+
+        for(char ch: t.toCharArray())
+        {
+            map.put(ch, map.getOrDefault(ch, 0 )  + 1);
+        }
+        int count = map.size();
+        int start = -1 , end = -1, size = (int)1e9;
+
+        for(int right = 0 ; right < s.length(); right++)
+        {
+            char ch = s.charAt(right);
+            if(!map.containsKey(ch)) continue;
+
+            map.put(ch, map.get(ch) - 1);
+                     
+            int frequency = map.get(ch);
+            if(frequency == 0) count--;
+
+            if(count == 0)
+            {
+                while(count == 0 && left <= right)
+                {
+                   int currSize = right - left + 1;
+                   if(currSize < size)
+                   {
+                       size = currSize;
+                       start = left;
+                       end = right;
+                   }
+                   char leftCh = s.charAt(left);
+                   if(map.containsKey(leftCh))
+                   {
+                       map.put(leftCh, map.get(leftCh) + 1);
+                   }
+                   else{
+                       left++;
+                       continue;
+                   }
+                   int freq = map.get(leftCh);
+                   if(freq > 0)
+                   {
+                       count++;
+                   }
+                   left++;
+                }
+            }
+        }
+        return ((start == -1 || end == -1) ? "" : s.substring(start , end + 1));
+    }
+}
