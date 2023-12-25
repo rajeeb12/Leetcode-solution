fe5b61b61bd3321aced4e51acc421f797a7123f8
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-25 11:47:34 +0530

    Time: 1 ms (84.19%) | Memory: 41.8 MB (9.46%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,9 +1,27 @@
 class Solution {
     int dp[];
     public int numDecodings(String s) {
+        int n = s.length();
+        if(n == 0) return 0;
+
         dp = new int[s.length()+1];
-        Arrays.fill(dp,-1);
-        return solve(0, s);
+        if(s.charAt(0)=='0') return 0;
+
+        dp[n] = 1;
+
+        for(int index = n-1; index >= 0; index--)
+        {
+            if(s.charAt(index) == '0') continue;
+            int oneLength = dp[index + 1];
+            int twoLength = 0;
+
+            if(index < s.length()-1 && Integer.parseInt(s.substring(index, index + 2)) <= 26)
+            {
+                twoLength = dp[index + 2];
+            }
+            dp[index] = oneLength+twoLength;
+        }
+        return dp[0];
     }
     public int solve(int index,String s)
     {
