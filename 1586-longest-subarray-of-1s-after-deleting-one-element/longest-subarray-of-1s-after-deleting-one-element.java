f888c4dc998c67a82d25f8895587968c054b195a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-12-28 08:11:19 +0530

    Time: 5 ms (6.68%) | Memory: 57.1 MB (6.17%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,18 +1,14 @@
 class Solution {
     public int longestSubarray(int[] nums) {
         int max = 0;
-        int countZero= 0;
+        int lastIndex= -1;
         int i = 0;
         for(int j = 0 ; j < nums.length; j++)
         {
-            if(nums[j] == 0) countZero++;
-            while(countZero > 1)
+            if(nums[j] == 0)
             {
-                if(nums[i] == 0)
-                {
-                    countZero--;
-                }
-                i++;
+                i = lastIndex + 1;
+                lastIndex = j;
             }
 
             max= Math.max(max, j - i); // window length aur ek element delete hoga for sure.
