7054ecea1e5aba3b7d80f29eaad21e2ba3e9d4e1
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-14 08:08:03 +0530

    Time: 10 ms (92.33%) | Memory: 45.7 MB (14.80%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int map1[]= new int[26];
-        int map2[]= new int[26];
-
         if(word1.length() != word2.length()) return false;
+        int map1[] = new int[27];
+        int map2[] = new int[27];
 
-        for(char ch: word1.toCharArray())
-        {
+        for(char ch : word1.toCharArray()){
             map1[ch-'a']++;
         }
-        for(char ch: word2.toCharArray())
-        {
+        for(char ch : word2.toCharArray()){
             map2[ch-'a']++;
         }
-        // option - 1
-        for(int i = 0 ; i < 26; i++)
+
+        for(int i = 0 ; i < 27; i++)
         {
-            if(map1[i] != 0 && map2[i] !=0) continue; // word1 aur word2 dono mein smae cahracter hai
-            if(map1[i] == 0 && map2[i] ==0) continue; // jo word1 mein nahi hai vo word2 mein bhi nahi hai
+            if(map1[i] == 0 && map2[i] == 0) continue;
+            if(map1[i] != 0 && map2[i] != 0) continue;// dono mein hai vo character so we can still swap and get the string
 
             return false;
         }
+
         Arrays.sort(map1);
         Arrays.sort(map2);
 
-        for(int i =0 ; i < 26 ; i++)
+        for(int i = 0 ; i < 27; i++)
         {
             if(map1[i] != map2[i]) return false;
         }
         return true;
-    }
+    } 
 }
