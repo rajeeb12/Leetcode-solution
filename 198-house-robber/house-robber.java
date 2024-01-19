26fb0510d6b8b9330bc60912b91e7c43825c8e85
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-20 02:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (22.65%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -7,31 +7,20 @@ class Solution {
         dp = new int[n + 1];
         
         dp[0] = nums[0];
+        int pp = 0;
+        int p = nums[0];
 
         for(int index = 1; index < n ; index++)
         {
             int rob = nums[index];
-            if(index > 1) rob += dp[index - 2];
-            int notRob = dp[index - 1];
+            if(index > 1) rob += pp;
+            int notRob = p;
 
-            dp[index] = Math.max(rob, notRob);
+            int cur = Math.max(rob, notRob);
+            pp = p;
+            p = cur;
         }
         
-        return dp[n - 1];
-    }
-    public int solve(int index, int[] nums)
-    {
-        if(index < 0) return 0;
-        if(index == 0)
-        {
-            return nums[0];
-        }
-
-        if(dp[index] != -1) return dp[index];
-
-        int rob = nums[index] + solve(index - 2, nums);
-        int notRob = solve(index - 1,nums);
-
-        return dp[index] = Math.max(rob, notRob);
+        return p;
     }
 }
