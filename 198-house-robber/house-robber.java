24a679a2781ebdaccf0ad0298305d9f6033c6f3e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-20 02:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (16.60%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,10 +2,22 @@ class Solution {
     int dp[];
     public int rob(int[] nums) {
         int n = nums.length; 
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
         dp = new int[n + 1];
-        Arrays.fill(dp, -1);
+        
+        dp[0] = nums[0];
 
-        return solve(n - 1, nums);
+        for(int index = 1; index < n ; index++)
+        {
+            int rob = nums[index];
+            if(index > 1) rob += dp[index - 2];
+            int notRob = dp[index - 1];
+
+            dp[index] = Math.max(rob, notRob);
+        }
+        
+        return dp[n - 1];
     }
     public int solve(int index, int[] nums)
     {
