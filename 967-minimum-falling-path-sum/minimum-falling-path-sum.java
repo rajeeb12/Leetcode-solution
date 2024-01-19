1726ff70e9c48d3007c4260b2ffe351c557cadbb
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-19 15:35:50 +0530

    Time: 8 ms (34.47%) | Memory: 45.1 MB (41.66%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,13 +1,13 @@
 class Solution {
-    int dp[][];
+    // int dp[][];
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        dp= new int[n + 1][n + 1];
+        // dp= new int[n + 1][n + 1];
         
-        for(int i = 0 ; i < n; i++)
-        {
-            dp[0][i] = matrix[0][i];
-        }
+        // for(int i = 0 ; i < n; i++)
+        // {
+        //     dp[0][i] = matrix[0][i];
+        // }
 
         int min = (int)1e9;
         // for(int col = 0 ; col < n; col++)
@@ -15,39 +15,79 @@ class Solution {
         //     min = Math.min(min, solve(n - 1, col, matrix, n));
         // }
 
-        for(int row = 1 ; row < n; row++)
+        // for(int row = 1 ; row < n; row++)
+        // {
+        //     for(int col = 0; col < n; col++)
+        //     {
+        //         int left =(int)1e9;
+        //         int right = (int)1e9;
+        //         if(col > 0 ) left = matrix[row][col] + dp[row - 1][col - 1];
+        //         if(col + 1 < n) right = matrix[row][col] + dp[row -1][col + 1];
+        //         int down = matrix[row][col] + dp[row - 1][col];
+
+        //         dp[row][col] = Math.min(left, Math.min(right, down));
+        //     }
+        // }
+        // for(int i = 0 ; i< n; i++)
+        // {
+        //     min = Math.min(dp[n-1][i], min);
+        // }
+
+        // space optimisation
+        int prev[] =new int[n];
+        
+
+        for(int i = 0 ; i < n; i++)
         {
+            prev[i] = matrix[0][i]; 
+        }
+        for(int row = 1 ; row < n ; row++)
+        {
+            int cur[]= new int[n];
             for(int col = 0; col < n; col++)
             {
-                int left =(int)1e9;
-                int right = (int)1e9;
-                if(col > 0 ) left = matrix[row][col] + dp[row - 1][col - 1];
-                if(col + 1 < n) right = matrix[row][col] + dp[row -1][col + 1];
-                int down = matrix[row][col] + dp[row - 1][col];
+                int left = matrix[row][col];
+                int right = matrix[row][col];
 
-                dp[row][col] = Math.min(left, Math.min(right, down));
+                if(col - 1 >= 0){
+                    left += prev[col - 1];
+                }else{
+                    left += (int)1e9;
+                }
+
+                if(col + 1 < n){
+                    right += prev[col + 1];
+                }else{
+                    right += (int)1e9;
+                }
+
+                int up = matrix[row][col] + prev[col];
+
+                cur[col] = Math.min(left, Math.min(right, up));
             }
+            prev = cur;
         }
-        for(int i = 0 ; i< n; i++)
+
+        for(int i = 0 ; i < n ; i++)
         {
-            min = Math.min(dp[n-1][i], min);
+            min = Math.min(prev[i], min);
         }
         return min;
     }
-    public int solve(int row,int col,int[][] matrix, int n)
-    {
-        if(col < 0 || col >= n) return (int)1e9;
-        if(row == 0)
-        {
-            return matrix[0][col];  
-        }
+    // public int solve(int row,int col,int[][] matrix, int n)
+    // {
+    //     if(col < 0 || col >= n) return (int)1e9;
+    //     if(row == 0)
+    //     {
+    //         return matrix[0][col];  
+    //     }
 
-        if(dp[row][col] != -1) return dp[row][col];
+    //     if(dp[row][col] != -1) return dp[row][col];
 
-        int left = matrix[row][col] + solve(row - 1, col - 1, matrix, n);
-        int right = matrix[row][col] + solve(row - 1, col + 1, matrix, n);
-        int down = matrix[row][col] + solve(row - 1, col , matrix, n);
+    //     int left = matrix[row][col] + solve(row - 1, col - 1, matrix, n);
+    //     int right = matrix[row][col] + solve(row - 1, col + 1, matrix, n);
+    //     int down = matrix[row][col] + solve(row - 1, col , matrix, n);
 
-        return dp[row][col] = Math.min(left, Math.min(right, down));
-    }
+    //     return dp[row][col] = Math.min(left, Math.min(right, down));
+    // }
 }
