e03344f26e2c705e801b9201c9e7ef68d992c38e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-21 03:04:55 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (11.60%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,43 +2,43 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int arr[] = new int[n1 + n2];
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int n = n1 + n2;
 
-        int i = 0 , j = 0;
-        int index = 0;
-        while(i < n1 && j < n2)
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) /2;
+
+        while(low <= high)
         {
-            if(nums1[i] < nums2[j])
+            int mid1 = (high + low) >> 1; // nums1
+            int mid2 = left - mid1; // nums2
+
+            int l1 = Integer.MIN_VALUE, l2= Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2= Integer.MAX_VALUE;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2]; 
+
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            
+            if(l1 <= r2 && l2 <= r1)
             {
-                arr[index] = nums1[i];
-                i++;
-            }else{
-                arr[index] = nums2[j];
-                j++;
+                if(n % 2 != 0)
+                {
+                    return Math.max(l1,l2);
+                }else{
+                    double cal = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                    return cal;
+                }
             }
-            index++;
+            else if(l1 > r2)
+            {
+                high = mid1 - 1;
+            }else{
+                low = mid1 + 1;
+            }
         }
-        while(i < n1)
-        {
-            arr[index] = nums1[i];
-            index++;
-            i++;
-        }
-        while(j < n2)
-        {
-            arr[index] = nums2[j];
-            j++;
-            index++;
-        }
-        if(arr.length % 2 == 0)
-        {
-            int half = arr.length / 2;
-            int sum = arr[half] + arr[half - 1];
-            System.out.print(sum);
-            return (double) sum / 2;
-        }else{
-            int half = arr.length / 2;
-            return arr[half];
-        }
+        return 0;
     }
 }
