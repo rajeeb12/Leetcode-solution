d71edc9e4e058586cf2bfc1b69dedef5e6dfc5d2
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-23 04:19:30 +0530

    Time: 2 ms (97.67%) | Memory: 46 MB (5.04%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int map[] = new int[n+1];
         for(int i: nums)
         {
-            map.put(i, map.getOrDefault(i, 0 ) + 1);
+            map[i]++;
         }
         int missing = 0;
         int repeated = 0;
         for(int i = 1 ; i <= n ; i++)
         {
-            if(map.containsKey(i) && map.get(i) == 2) repeated = i;
-            if(!map.containsKey(i)) missing = i; 
+            if(map[i] == 2) repeated = i;
+            if(map[i] == 0) missing = i; 
         }
         return new int[]{repeated, missing};
     }
