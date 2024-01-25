0160767c964a01a585f608a3d3d5d88ab4c4f421
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-25 14:58:15 +0530

    Time: 4 ms (30.97%) | Memory: 45.3 MB (45.72%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,10 +3,37 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        dp = new int[n + 1][m + 1];
-        for(int i[] : dp) Arrays.fill(i , - 1);
+        //dp = new int[n + 1][m + 1];
+        int prev[]= new int[m + 1];
 
-        return solve(grid, n -1 , m -1);
+        for(int i = 0 ; i < n ; i++)
+        {
+            int cur[]= new int[m + 1];
+            for(int j = 0 ; j < m ; j++)
+            {
+                if(i == 0 & j == 0) cur[j] = grid[i][j];
+                else{
+                    int up= grid[i][j];
+                    int left = grid[i][j]; 
+                    if(i > 0)
+                    {
+                        up += prev[j];
+                    }else{
+                        up += (int)1e9;
+                    }
+
+                    if(j > 0){
+                        left += cur[j - 1];
+                    }else{
+                        left += (int)1e9;
+                    }
+                    cur[j] = Math.min(up, left);
+                }
+            }
+            prev = cur;
+        }
+        //return solve(grid, n -1 , m -1);
+        return prev[m - 1];
     }
     public int solve(int[][] grid,int i, int j)
     {
