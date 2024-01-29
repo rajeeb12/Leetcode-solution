cead2c228ec8ed2c5125031ee4059f43b0039672
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-30 01:28:48 +0530

    Time: 10 ms (91.90%) | Memory: 49.3 MB (9.88%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,54 +1,61 @@
 class Pair{
+    int v;
     int time;
-    int node;
-    public Pair(int _t,int _n){
+    public Pair(int _v,int _t)
+    {
+        this.v = _v;
         this.time = _t;
-        this.node = _n;
     }
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-
-        for(int i = 0 ; i <= n ; i++)
+        for(int i = 0 ; i <=n; i++)
         {
             adj.add(new ArrayList<>());
         }
-        for(int time[]: times){
+
+        for(int time[]: times)
+        {
             int u = time[0];
             int v = time[1];
             int t = time[2];
-            adj.get(u).add(new Pair(t, v));
+
+            adj.get(u).add(new Pair(v, t));
         }
+        int dist[] = new int[n + 1];
+        Arrays.fill(dist, (int)1e9);
         Queue<Pair> q = new LinkedList<>();
-        int ans[] = new int[n + 1];
-        
-        Arrays.fill(ans, (int)1e9);
-        q.add(new Pair(0,k));
-        ans[k] = 0;
+        q.add(new Pair(k,0));
+        dist[k]=0;
 
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int node = p.node;
-            int time = p.time;
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i = 0 ; i < size; i++)
+            {
+                Pair p = q.poll();
+                int node = p.v;
+                int time = p.time;
 
-            for(Pair t: adj.get(node)){
-                int adjNode = t.node;
-                int adjTime = t.time;
-                if(adjTime + time < ans[adjNode]){
-                    ans[adjNode] = adjTime + time;
-                    q.add(new Pair(ans[adjNode] , adjNode));
+                for(Pair t: adj.get(node))
+                {
+                    int adjNode = t.v;
+                    int adjTime = t.time;
+                    if(adjTime + time < dist[adjNode])
+                    {
+                        dist[adjNode] = adjTime + time;
+                        q.add(new Pair(adjNode, dist[adjNode]));
+                    }
                 }
             }
-        } 
-        int max = 0;
-        for(int i = 1; i<=n; i++)
-        {
-            if(ans[i] ==(int)1e9){
-                return -1;
-            }
-            max = Math.max(max, ans[i]);
         }
-        return max;
+        int ans = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(dist[i] == (int)1e9) return -1;
+            ans =Math.max(ans, dist[i]);
+        }
+        return ans;
     }
 }
