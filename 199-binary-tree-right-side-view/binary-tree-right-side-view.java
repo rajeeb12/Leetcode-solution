540a4200f42cb7e46a3c4164257a90b69b68b030
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-31 02:30:42 +0530

    Time: 2 ms (11.09%) | Memory: 42.1 MB (36.60%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,36 +16,41 @@
 class Pair{
     int level;
     TreeNode node;
-    public Pair(int _l, TreeNode _n){
+    public Pair(int _l,TreeNode _n)
+    {
         this.level = _l;
         this.node = _n;
     }
 }
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+
+        if(root == null) return ans;
         Queue<Pair> q = new LinkedList<>();
-        List<Integer> res= new ArrayList<>();
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        if(root== null) return res;
+        q.add(new Pair(0, root));
 
-        q.add(new Pair(0,root));
+        while(!q.isEmpty())
+        {
+            Pair temp = q.poll();
+            int level = temp.level;
+            TreeNode node = temp.node;
 
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i= 0 ; i < size ;i++){
-                Pair temp = q.poll();
-                int level = temp.level;
-                TreeNode node = temp.node;
-
-                map.put(level, node.val);
-                if(node.left != null) q.add(new Pair(level + 1 , node.left));
-                 if(node.right != null) q.add(new Pair(level + 1 , node.right));
+            map.put(level, node.val);
+            if(node.left != null)
+            {
+                q.add(new Pair(level + 1, node.left));
+            }
+            if(node.right != null)
+            {
+                q.add(new Pair(level + 1, node.right));
             }
         }
-        for(int value : map.values()){
-            res.add(value);
+        for(Map.Entry<Integer,Integer> entry: map.entrySet())
+        {
+            ans.add(entry.getValue());
         }
-        return res;
+        return ans;
     }
 }
