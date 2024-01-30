07a0ed4683c73097a2259b181dafc839046cff27
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-01-30 23:24:32 +0530

    Time: 44 ms (71.76%) | Memory: 113.9 MB (65.70%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,62 +1,72 @@
 class Node{
     int key;
-    int val;
+    int value;
     Node prev;
     Node next;
-    Node(int k,int v)
+    public Node(int _k,int _v)
     {
-        this.key = k;
-        this.val = v;
+        this.key = _k;
+        this.value = _v;
+        prev = null;
+        next = null;
     }
 }
 class LRUCache {
-    HashMap<Integer,Node> map;
-    Node head = new Node(0,0);
-    Node tail = new Node(0,0);;
-    int cap;
+    HashMap<Integer, Node> map;
+    Node head;
+    Node tail;
+    int size;
     public LRUCache(int capacity) {
-        cap = capacity;
         map = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
         head.next = tail;
         tail.prev = head;
+        size = capacity;
     }
     
     public int get(int key) 
     {
-        if(!map.containsKey(key)) return -1;
-
-        Node cur = map.get(key);
-        remove(cur);
-        insert(new Node(key ,cur.val));
-        return cur.val;
+        if(map.containsKey(key))
+        {
+            Node node = map.remove(key);
+            remove(node);
+            insert(new Node(key, node.value));
+            return node.value;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key))
         {
-            remove(map.get(key));
+            remove(map.remove(key));
         }
-        if(map.size() == cap)
-        {
-            remove(tail.prev);
+        if(map.size() >= size){
+            Node node = tail.prev;
+            map.remove(node.key);
+            remove(node);
         }
         insert(new Node(key, value));
     }
-    public void remove(Node node)
-    {
-        map.remove(node.key);
-        Node next = node.next;
-        node.prev.next = next;
-        node.next.prev = node.prev;
-    }
-    public void insert(Node newNode)
+    public void insert(Node node)
     {
         Node next = head.next;
-        head.next = newNode;
-        newNode.next = next;
-        newNode.next.prev = newNode;
-        newNode.prev = head;
-        map.put(newNode.key,newNode);
+        head.next = node;
+        node.next = next;
+        node.next.prev = node;
+        node.prev = head;
+        map.put(node.key, node);
+    }
+    public void remove(Node node)
+    {
+
+        Node next = node.next;
+        Node prev = node.prev;
+        node.next.prev = prev;
+        node.prev.next = next;
+        node.prev = null;
+        node.next = null;
     }
 }
 
