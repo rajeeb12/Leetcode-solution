ea0eb647f2bb56adf994f9f8ee2364fd5b72f405
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-01 02:04:48 +0530

    Time: 33 ms (45.57%) | Memory: 55.3 MB (15.50%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,45 +1,42 @@
 class Tuple{
-    int dist;
+    int value;
     int x;
     int y;
-    public Tuple(int _d,int _x,int _y)
+    public Tuple(int _v,int _x,int _y)
     {
-        this.dist = _d;
+        this.value = _v;
         this.x = _x;
         this.y = _y;
     }
 }
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Tuple> pq= new PriorityQueue<>((a,b) -> b.dist - a.dist);
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b) -> b.value - a.value);
 
         for(int i[]: points)
         {
             int x = i[0];
             int y = i[1];
-            int dist = (int) ((x * x) + (y * y));
-            pq.add(new Tuple(dist, x ,y));
 
-            if(pq.size() > k)
+            int value = x * x + y * y;
+            pq.add(new Tuple(value, x, y));
+
+            while(!pq.isEmpty() && pq.size() > k)
             {
                 pq.poll();
             }
         }
-        int ans[][]= new int[k][2];
-
+        int ans[][] = new int[pq.size()][2];
         int index = 0;
-
         while(!pq.isEmpty())
         {
-            Tuple t = pq.poll();
-            int x = t.x;
-            int y = t.y;
+            Tuple temp= pq.poll();
+            int x = temp.x;
+            int y = temp.y;
             ans[index][0] = x;
             ans[index][1] = y;
             index++;
         }
-
         return ans;
-
     }
 }
