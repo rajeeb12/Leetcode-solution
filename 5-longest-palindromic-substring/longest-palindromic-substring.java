140ce7e6a7d199da844b57fc1f29748dadc522c0
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-06 02:41:37 +0530

    Time: 17 ms (77.53%) | Memory: 41.9 MB (85.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,43 +1,40 @@
 class Solution {
     public String longestPalindrome(String s) {
-
-        int n = s.length();
-        if(n < 2) return s;
-        int max = 0;
         int start = 0;
         int end = 0;
-
-        for(int i = 0 ; i < n ; i++)
+        int len = 0;
+        int n = s.length();
+        for(int i = 0; i < n ; i++)
         {
-            // odd length
             int left = i, right = i;
-            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right))
+            while(left >= 0 && right < n && s.charAt(left) ==s.charAt(right))
             {
-                int sum = right - left + 1;
-                if(max < sum)
+                int curmax = right - left + 1; 
+                if(len < curmax)
                 {
-                    max = sum;
+                    len = right - left + 1;
                     start = left;
-                    end = right + 1;
+                    end = right;
                 }
                 left--;
                 right++;
             }
-            //even
-            left = i; right = i + 1;
-            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right))
+            
+            left = i ; right = i + 1;
+            while(left >= 0 && right < n && s.charAt(left) ==s.charAt(right))
             {
-                int sum = right - left + 1;
-                if(max < sum)
+                int curmax = right - left + 1; 
+                if(len < curmax)
                 {
-                    max = sum;
+                    len = right - left + 1;
                     start = left;
-                    end = right + 1;
+                    end = right;
                 }
                 left--;
                 right++;
             }
+            
         }
-        return s.substring(start, end);
+        return s.substring(start,end + 1);
     }
 }
