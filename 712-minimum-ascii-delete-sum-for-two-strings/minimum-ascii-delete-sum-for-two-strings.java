5840540ed2697783e59fa4bd1e8695e778881725
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-05 22:58:11 +0530

    Time: 24 ms (45.51%) | Memory: 44.9 MB (33.24%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -4,11 +4,38 @@ class Solution {
         int n1 = s1.length();
         int n2 = s2.length();
         dp = new int[n1 + 1][n2 + 1];
-        for(int i[]: dp)
+
+        for(int index1 = 0; index1 <= n1 ; index1++)
         {
-            Arrays.fill(i, - 1);
+            for(int index2 = 0; index2 <= n2; index2++)
+            {
+                if(index1 == 0) dp[index1][index2]= removeAll(s2,index2);
+                if(index2 == 0) dp[index1][index2]= removeAll(s1,index1);
+                else if(index1 != 0){
+                    int notsame = (int)1e9;
+                    if(s1.charAt(index1 - 1) == s2.charAt(index2 -1)){
+                        dp[index1][index2] = dp[index1 - 1][index2 - 1];
+                        continue;
+                    }
+                
+                    int ans1 = s1.charAt(index1 - 1) + dp[index1 - 1][index2];
+                    int ans2 = s2.charAt(index2 - 1) + dp[index1][index2 - 1];
+                    notsame = Math.min(ans1, ans2);
+                    dp[index1][index2]= notsame;
+                }
+            }
         }
-        return solve(s1, s2,s1.length() - 1,s2.length() -1);
+        return dp[n1 ][n2 ];
+        //return solve(s1, s2,s1.length() - 1,s2.length() -1);
+    }
+    public int removeAll(String s,int len)
+    {
+        int total =0;
+        for(int i = 0 ; i < len; i++)
+        {
+            total += (int)s.charAt(i);
+        }
+        return total;
     }
     public int solve(String s1, String s2,int index1,int index2)
     {
