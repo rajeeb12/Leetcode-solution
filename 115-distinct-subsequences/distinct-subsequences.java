11e5165bbbf85afa5f4d09b06b08680a08d96f8c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-06 19:29:42 +0530

    Time: 14 ms (71.84%) | Memory: 44.7 MB (90.33%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,27 +3,33 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        dp = new int[n + 1][m + 1];
+        //dp = new int[n + 1][m + 1];
+        int prev[] = new int[m + 1];
         
-        for(int i = 0 ; i <= n; i++)
-        {
-            dp[i][0] = 1;
-        }
+        // for(int i = 0 ; i <= n; i++)
+        // {
+        //     // dp[i][0] = 1;
+        // }
+        prev[0] = 1;
 
         for(int i = 1; i <= n; i++)
         {
+            int cur[] = new int[m + 1];
+            cur[0]= 1;
             for(int j = 1; j <= m; j++)
             {
                 if(s.charAt(i - 1) == t.charAt(j - 1))
                 {
-                    dp[i][j]= (dp[i - 1][j] + dp[i - 1][j - 1]);
+                    cur[j]= (prev[j] + prev[j - 1]);
                 }
                 else{
-                    dp[i][j] = dp[i - 1][j];
+                    cur[j] = prev[j];
                 }
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
+        ///return dp[n][m];
         //return solve(n - 1,  m - 1,s,t);   
     }
     public int solve(int i,int j,String s,String t)
