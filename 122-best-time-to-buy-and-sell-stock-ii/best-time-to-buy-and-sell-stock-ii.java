d422e6e7883357cdfe7183ab62781e6f1690ec3d
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-07 03:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (32.25%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,40 +1,44 @@
 class Solution {
-    int dp[][];
+    //int dp[][];
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new int[n + 1][2];
-        
+        //dp = new int[n + 1][2];
+        int cur[]= new int[2];
+        int next[] = new int[2];  
         for(int i = n - 1; i >=0 ; i--)
         {
             for(int buyOrSell = 0 ; buyOrSell <= 1; buyOrSell++)
             {
+                int profit = 0;
                 if(buyOrSell == 1)
                 {
-                    dp[i][buyOrSell] =Math.max(-prices[i] + dp[i + 1][1 -  buyOrSell] , dp[i + 1][ buyOrSell]);
+                    profit = Math.max(-prices[i] + next[1 - buyOrSell] , next[buyOrSell]);
                 }
                 else{
-                    dp[i][buyOrSell]= Math.max(prices[i] + dp[i + 1][1 - buyOrSell] , dp[i + 1][ buyOrSell]);
+                    profit = Math.max(prices[i] + next[1 -buyOrSell] , next[buyOrSell]);
                 }
+                cur[buyOrSell] = profit;
             }
+            next = cur;
         }
-        return dp[0][1];
+        return cur[1];
         //return solve(0, prices, 1);
     }
-    public int solve(int i, int prices[], int buyOrSell)
-    {
-        int n = prices.length; 
-        if(i == n)
-        {
-            return 0;
-        }
-        if(dp[i][buyOrSell] != -1) return dp[i][buyOrSell];
+    // public int solve(int i, int prices[], int buyOrSell)
+    // {
+    //     int n = prices.length; 
+    //     if(i == n)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i][buyOrSell] != -1) return dp[i][buyOrSell];
 
-        if(buyOrSell == 1)
-        {
-            return dp[i][buyOrSell] =Math.max(-prices[i] + solve(i + 1, prices, 1 - buyOrSell) , solve(i + 1, prices,buyOrSell));
-        }
-        else{
-            return dp[i][buyOrSell]= Math.max(prices[i] + solve(i + 1, prices, 1 - buyOrSell) , solve(i + 1, prices, buyOrSell));
-        }
-    }
+    //     if(buyOrSell == 1)
+    //     {
+    //         return dp[i][buyOrSell] =Math.max(-prices[i] + solve(i + 1, prices, 1 - buyOrSell) , solve(i + 1, prices,buyOrSell));
+    //     }
+    //     else{
+    //         return dp[i][buyOrSell]= Math.max(prices[i] + solve(i + 1, prices, 1 - buyOrSell) , solve(i + 1, prices, buyOrSell));
+    //     }
+    // }
 }
