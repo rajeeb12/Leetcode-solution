b4518171b31c9622fdd2dd8e5cb59810ba6a63ff
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-08 02:57:37 +0530

    Time: 385 ms (7.27%) | Memory: 77.1 MB (10.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,26 +1,23 @@
 class Solution {
+    int n;
     int dp[][];
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        dp = new int[n + 1][n + 1];
-        
-        for(int i[]: dp) Arrays.fill(i, - 1);
-
-        return helper(0,-1,nums, n);
+        n = nums.length;
+        dp = new int[n +1][n+1];
+        for(int i[]: dp) Arrays.fill(i, -1);
+        return solve(0,-1,nums);
     }
-    public int helper(int index,int prev_index,int nums[],int n)
+    public int solve(int i,int pi,int[] nums)
     {
-        if(index == n) return 0;
-        
-        if(dp[index][prev_index + 1] != -1) return dp[index][prev_index + 1];
+        if(i == n) return 0;
+        if(dp[i][pi + 1] != -1) return dp[i][pi + 1];
 
-        int not_take = helper(index + 1, prev_index,nums, n);
+        int notTake = 0 + solve(i + 1, pi, nums);
         int take = 0;
-
-        if(prev_index == -1 || nums[index] > nums[prev_index])
+        if(pi == -1 || (nums[i] > nums[pi]))
         {
-            take = 1 + helper(index + 1, index , nums, n);
-        } 
-        return dp[index][prev_index + 1] = Math.max(take, not_take);
+            take = 1 + solve(i + 1, i, nums);
+        }
+        return dp[i][pi + 1]= Math.max(notTake, take);
     }
 }
