69c4b9e3e3455ff937e139f8243153d0e8459a79
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-09 13:26:23 +0530

    Time: 14 ms (75.44%) | Memory: 43 MB (49.96%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -3,32 +3,38 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int dp[] = new int[n + 1];
-        Arrays.fill(dp, 1);
+        Arrays.fill(dp,1);
         int hash[] = new int[n + 1];
         int max = 1;
         int start = 0;
-        for(int i = 0 ; i < n; i++)
+
+        for(int i = 0; i < n; i++)
         {
             hash[i] = i;
-            for(int prevIdx = 0; prevIdx < i; prevIdx++)
+            for(int pi = 0; pi < i; pi++)
             {
-                if( nums[i] % nums[prevIdx]  == 0  && dp[i] < dp[prevIdx] + 1)
+                if(nums[i] % nums[pi] == 0)
                 {
-                    dp[i] = 1 + dp[prevIdx]; 
-                    hash[i] = prevIdx;
-                    if(dp[i] > max)
+                    if(dp[i] < dp[pi] + 1)
                     {
-                        max = dp[i];
-                        start = i;
+                        dp[i] = dp[pi] + 1;
+                        hash[i] = pi;
+                        if(max < dp[i])
+                        {
+                            max = dp[i];
+                            start = i;
+                        }
                     }
                 }
             }
-        }
+        } 
         List<Integer> ans = new ArrayList<>();
+
         ans.add(nums[start]);
+
         while(hash[start] != start)
         {
-            ans.add(nums[hash[start]]);
+            ans.add(0,nums[hash[start]]);
             start = hash[start];
         }
         return ans;
