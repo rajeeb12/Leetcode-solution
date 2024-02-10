a8c07ae24dd6ffd40937ac6482a9ead8d164830c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-10 14:12:49 +0530

    Time: 13 ms (28.53%) | Memory: 45.1 MB (24.39%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,10 +1,10 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
-        boolean dp[][] = new boolean[n][n];
-        int count = 0;
+        boolean dp[][] = new boolean[n + 1][n + 1];
+        int count = 0; 
 
-        for(int g = 0 ; g < n; g++)
+        for(int g = 0 ; g < n ; g++)
         {
             for(int i = 0, j = g; j < n ; i++, j++)
             {
@@ -14,12 +14,11 @@ class Solution {
                 }
                 else if(g == 1)
                 {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                }
-                else{
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                }else{
                     if(s.charAt(i) == s.charAt(j))
                     {
-                        dp[i][j] = dp[i+1][j - 1];
+                        dp[i][j] = dp[i + 1][j - 1];
                     }
                 }
                 if(dp[i][j])
