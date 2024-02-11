33d38cca61d2c06f02a4b3b5f5643b8fe3fb7ac5
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-12 04:04:33 +0530

    Time: 20 ms (88.03%) | Memory: 45.6 MB (74.50%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -4,14 +4,36 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         dp = new int[n + 1][m + 1][m + 1];
-        for(int i[][]: dp)
+        
+        for(int i = n - 1; i >= 0; i--)
         {
-            for(int j[]: i)
+            for(int j1 = m - 1; j1 >=0 ; j1--)
             {
-                Arrays.fill(j, -1);
+                for(int j2 = 0; j2 < m; j2++) 
+                {
+                    if(j2 == 0) continue;
+                    int res = 0;
+                    res += grid[i][j1];
+                    res += grid[i][j2];
+                    
+                    int max = 0;
+                    for(int k1 = j1- 1; k1 <= j1 + 1; k1++)
+                    {
+                        for(int k2 = j2 - 1; k2 <= j2 + 1; k2++)
+                        {
+                            if(k1 < k2 && k2 >= 0 && k1 >= 0)
+                            {
+                                max =  Math.max(max, dp[i + 1][k1][k2]);
+                            }
+                        }
+                    }
+                    res += max;
+                    dp[i][j1][j2]= res;
+                }
             }
         }
-        return solve(0,0,m-1,grid);
+        return dp[0][0][m-1];
+        //return solve(0,0,m-1,grid);
     }
     public int solve(int i,int j1,int j2,int[][] grid)
     {
@@ -19,7 +41,7 @@ class Solution {
 
         if(i == grid.length) return 0;
 
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2]; 
 
         int res = 0;
         res += grid[i][j1];
