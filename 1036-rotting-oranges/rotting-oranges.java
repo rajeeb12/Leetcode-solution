3c29ed6a9d6af1caacdd3e7dcafd3381bea26903
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-14 20:47:13 +0530

    Time: 2 ms (76.43%) | Memory: 42.3 MB (62.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,51 +1,75 @@
+class Pair{
+    int row;
+    int col;
+    public Pair(int _r,int _c)
+    {
+        this.row = _r;
+        this.col = _c;
+    }
+}
 class Solution {
+    int n;
+    int m;
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> q= new LinkedList<>();
-        int n =  grid.length;
-        int m = grid[0].length;
-        int countOranges = 0;
+        n = grid.length;
+        m = grid[0].length;
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j=0; j < grid[0].length; j++){
-                if(grid[i][j] == 2){ // storing position of 2 in queue
-                    q.add(new int[]{i,j});
-                }
-                if(grid[i][j] != 0){ 
-                    countOranges++;
+        Queue<Pair> q = new LinkedList<>();
+        int visited[][] = new int[n][m];
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < m; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.add(new Pair(i,j));
+                    visited[i][j] = 1;
                 }
             }
-        } 
-        if(countOranges == 0) return 0;
-
-        int di[]= {-1,1,0,0};
-        int dj[]= {0,0,-1,1};
-        int minTime = 0;
-        int rotten = 0;
-
+        }
+        int di[]= {1,-1,0,0};
+        int dj[]= {0,0,1,-1};
+        int time = 0;
         while(!q.isEmpty())
         {
             int size = q.size();
-            rotten += size;
-            for(int i = 0 ; i < size; i++)
-            {
-                int temp[] = q.poll();
-                int x =temp[0];
-                int y =temp[1];
-                for(int j = 0 ; j < 4; j++)
-                {
-                    int nextX = x + di[j];
-                    int nextY = y + dj[j];
 
-                    if(nextX >=0 && nextY >= 0 && nextX < n && nextY < m && grid[nextX][nextY] != 2 && grid[nextX][nextY] != 0){
-                        grid[nextX][nextY] = 2;
-                        q.add(new int[]{nextX, nextY});
+            for(int i = 0; i < size; i++)
+            {
+                Pair p = q.poll();
+                int r = p.row;
+                int c = p.col;
+
+                for(int k = 0; k < 4; k++)
+                {
+                    int row = r + di[k];
+                    int col = c + dj[k];
+                    if(check(row, col) && grid[row][col] == 1 && visited[row][col] == 0)
+                    {
+                        grid[row][col] = 2;
+                        visited[row][col] = 1;
+                        q.add(new Pair(row, col));
                     }
                 }
             }
-            minTime++;
+            time++;
         }
-        System.out.print(rotten +""+countOranges);
-        return (rotten == countOranges ? minTime - 1 : -1);
-
+        for(int i = 0 ; i < n; i++)
+        {
+            for(int j = 0 ; j < m; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    return -1;
+                }
+            }
+        }
+        return (time == 0 ? 0 : time - 1);
+    }
+    public boolean check(int i,int j)
+    {
+        if(i < 0 || j < 0 || i >= n || j >= m) return false;
+        return true;
     }
 }
