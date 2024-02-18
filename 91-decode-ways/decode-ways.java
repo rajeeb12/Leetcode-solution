ff95a7d50d1f0d15ea9d9dc6bf31b00ea381b8b7
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-19 03:38:08 +0530

    Time: 1 ms (79.08%) | Memory: 41.8 MB (47.16%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,46 +2,29 @@ class Solution {
     int dp[];
     public int numDecodings(String s) {
         int n = s.length();
-        if(n == 0) return 0;
-
-        dp = new int[s.length()+1];
-        if(s.charAt(0)=='0') return 0;
-
-        dp[n] = 1;
-
-        for(int index = n-1; index >= 0; index--)
-        {
-            if(s.charAt(index) == '0') continue;
-            int oneLength = dp[index + 1];
-            int twoLength = 0;
-
-            if(index < s.length()-1 && Integer.parseInt(s.substring(index, index + 2)) <= 26)
-            {
-                twoLength = dp[index + 2];
-            }
-            dp[index] = oneLength+twoLength;
-        }
-        return dp[0];
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(0,s);
     }
     public int solve(int index,String s)
     {
-        if(index == s.length())
+        if(index >= s.length())
         {
             return 1;
         }
+        if(dp[index] != -1) return dp[index];
+
         if(s.charAt(index) == '0')
         {
             return 0;
         }
-        if(dp[index] != -1) return dp[index];
-        int oneLength = solve(index + 1,s);
-        int twoLength = 0;
+        int ans1 = solve(index + 1, s);
+        int ans2 = 0;
 
-        if(index < s.length()-1 && Integer.parseInt(s.substring(index, index + 2)) <= 26)
+        if(index < s.length()-1 && Integer.parseInt(s.substring(index , index + 2)) <= 26)
         {
-            twoLength = solve(index + 2,s);
+            ans2 = solve(index + 2, s);
         }
-        return dp[index] = oneLength+twoLength;
-        
+        return dp[index]= ans1 + ans2;
     }
 }
