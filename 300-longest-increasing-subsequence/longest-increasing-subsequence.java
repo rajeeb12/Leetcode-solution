6dcdf5d455b50681ecfaec8ec0d7e1acf9848970
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-19 21:58:20 +0530

    Time: 381 ms (7.94%) | Memory: 77.4 MB (9.93%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,29 @@
 class Solution {
+    int dp[][];
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n +1];
-        Arrays.fill(dp, 1);
-        int max = 1;
-        for(int i = 0 ; i < n ; i++)
+        dp = new int[n + 1][n + 1];
+        for(int i[]: dp)
         {
-            for(int j = 0 ; j < i; j++)
-            {
-                if(nums[j] < nums[i])
-                {
-                    dp[i] = Math.max(1 + dp[j] , dp[i]);
-                    max = Math.max(dp[i], max);
-                }
-            }
+            Arrays.fill(i, -1);
         }
-        return max;
-        
+        return solve(0, -1, nums);
+    }
+    public int solve(int index,int prev,int[] nums)
+    {
+        if(index == nums.length)
+        {
+            return 0;
+        }
+        if(dp[index][prev + 1] != -1) return dp[index][prev + 1];
+
+        int not_pick = solve(index + 1, prev, nums);
+        int pick = 0;
+
+        if(prev == -1 || (nums[index] > nums[prev]))
+        {
+            pick = 1 + solve(index + 1, index, nums);
+        } 
+        return dp[index][prev + 1] = Math.max(pick , not_pick);
     }
 }
