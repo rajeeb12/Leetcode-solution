6334801c9ba4507cc377334987ce387c6c5380da
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-19 15:13:39 +0530

    Time: 1 ms (49.23%) | Memory: 40.8 MB (56.93%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,7 +3,18 @@ class Solution {
         if(n == 1) return true;
         if(n < 0) return false;
 
-        int bitcount = Integer.bitCount(n);
+        int bitcount = getCount(n);
         return (bitcount == 1);
+    }
+    public int getCount(int n)
+    {
+        int count = 0;
+
+        while(n != 0)
+        {
+            count += (n & 1);
+            n >>>= 1;
+        }
+        return count;
     }
 }
