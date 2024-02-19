2ed8d4d6b30e8c79e6569b6c81a4328df5106e8a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-19 22:24:18 +0530

    Time: 41 ms (56.25%) | Memory: 44.2 MB (44.05%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,44 +1,42 @@
 class Solution {
-    int dp[][];
+    int dp[];
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        //dp = new int[n + 1][n + 1];
-        int[] cur = new int[n + 1];
-        int[] next = new int[n + 1];
-        
-        for(int index = n - 1; index >= 0 ;index--)
+        dp = new int[n + 1];
+        Arrays.fill(dp, 1);
+        // int[] cur = new int[n + 1];
+        // int[] next = new int[n + 1];
+        int max = 1;
+        for(int index = 0; index < n ;index++)
         {
-            for(int prev = index - 1; prev >= -1 ; prev--)
+            for(int prev = 0; prev < index ; prev++)
             {
-                int not_pick = next[prev + 1] ;
-                int pick = 0;
-
-                if(prev == -1 || (nums[index] > nums[prev]))
+                if(nums[index] > nums[prev])
                 {
-                    pick = 1 + next[index + 1];
+                    dp[index] = Math.max(1 + dp[prev], dp[index]);
+                    max = Math.max(dp[index], max);
                 } 
-                cur[prev + 1] = Math.max(pick , not_pick);
             }
-            next = cur;
         }
-        return cur[0];
+        return max;
+        //return cur[0];
         //return solve(0, -1, nums);
     }
-    public int solve(int index,int prev,int[] nums)
-    {
-        if(index == nums.length)
-        {
-            return 0;
-        }
-        if(dp[index][prev + 1] != -1) return dp[index][prev + 1];
+    // public int solve(int index,int prev,int[] nums)
+    // {
+    //     if(index == nums.length)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[index][prev + 1] != -1) return dp[index][prev + 1];
 
-        int not_pick = solve(index + 1, prev, nums);
-        int pick = 0;
+    //     int not_pick = solve(index + 1, prev, nums);
+    //     int pick = 0;
 
-        if(prev == -1 || (nums[index] > nums[prev]))
-        {
-            pick = 1 + solve(index + 1, index, nums);
-        } 
-        return dp[index][prev + 1] = Math.max(pick , not_pick);
-    }
+    //     if(prev == -1 || (nums[index] > nums[prev]))
+    //     {
+    //         pick = 1 + solve(index + 1, index, nums);
+    //     } 
+    //     return dp[index][prev + 1] = Math.max(pick , not_pick);
+    // }
 }
