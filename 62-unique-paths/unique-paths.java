862ab2a9810e21bd0c3a1a2a6054dabb4b7369b7
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-20 02:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (66.71%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,9 +2,27 @@ class Solution {
     int dp[][];
     public int uniquePaths(int m, int n) {
         dp = new int[m + 1][n + 1];
-        for(int i[]: dp) Arrays.fill(i, -1);
+        
 
-        return solve(m - 1, n - 1);
+        for(int row = 0 ; row < m; row++)
+        {
+            for(int col = 0 ; col < n; col++)
+            {
+                if(row == 0 && col == 0)
+                {
+                    dp[row][col] = 1;
+                    continue;
+                }
+                int sum = 0;
+                 
+                if(row > 0) sum += dp[row - 1][col];
+                if(col > 0) sum += dp[row][col - 1];
+
+                dp[row][col] = sum;
+            }
+        }
+        return dp[m - 1][n - 1];
+        //return solve(m - 1, n - 1);
     }
     public int solve(int row, int col)
     {
