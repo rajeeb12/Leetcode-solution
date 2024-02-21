c3d7440a05d0f6c217022b2f9bc98ac77dbfe28c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-22 03:46:53 +0530

    Time: 13 ms (65.14%) | Memory: 44.9 MB (52.58%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,47 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()) return "";
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0 ;
+        HashMap<Character,Integer> map = new HashMap<>();
 
         for(char ch: t.toCharArray())
         {
-            map.put(ch, map.getOrDefault(ch, 0 )  + 1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        int count = map.size();
-        int start = -1 , end = -1, size = (int)1e9;
 
-        for(int right = 0 ; right < s.length(); right++)
+        int start = -1 , end = -1, minLength = (int)1e9, count =map.size(),i = 0;
+
+        for(int j = 0 ; j < s.length(); j++)
         {
-            char ch = s.charAt(right);
+            char ch = s.charAt(j);
             if(!map.containsKey(ch)) continue;
-
+            
             map.put(ch, map.get(ch) - 1);
-            int frequency = map.get(ch);
-
-            if(frequency == 0) count--;
-
-            for(left = left ; count == 0 && left <= right ; left++)
+            int freq = map.get(ch);
+            if(freq == 0)
             {
-               int currSize = right - left + 1;
-               if(currSize < size)
-               {
-                   size = currSize;
-                   start = left;
-                   end = right;
-               }
-               char leftCh = s.charAt(left);
-               if(map.containsKey(leftCh))
-               {
-                    map.put(leftCh, map.get(leftCh) + 1);
-                    int freq = map.get(leftCh);
-                    if(freq > 0)
+                count--;
+            }
+            for(i = i ; count == 0 && i <= j; i++)
+            {
+                int curLength = j - i + 1;
+                if(minLength > curLength)
+                {
+                    start = i;
+                    end = j;
+                    minLength = curLength;
+                }
+                char ch1 = s.charAt(i);
+                if(map.containsKey(ch1))
+                {
+                    map.put(ch1, map.get(ch1) + 1);
+
+                    int freq2= map.get(ch1);
+                    if(freq2 > 0)
                     {
                         count++;
                     }
-               }
+                }
             }
         }
-        return ((start == -1 || end == -1) ? "" : s.substring(start , end + 1));
+        return (start == -1 || end == -1 ? "": s.substring(start, end + 1));
     }
 }
