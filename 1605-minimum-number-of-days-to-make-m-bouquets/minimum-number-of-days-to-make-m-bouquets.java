4d21d2fb3539efa1598c482ea1383f418ccf7c90
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-28 21:58:41 +0530

    Time: 22 ms (42.05%) | Memory: 58.8 MB (10.73%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,38 +1,42 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        long val = (long) m * (long) k;
-        if(val > bloomDay.length) return -1;
-        int low = (int)1e9;
-        int high = -(int)1e9;
-
-        for(int i: bloomDay){
-            low = Math.min(i, low);
-            high = Math.max(i, high);
-        }
+        int max = 0;
+        int min = Integer.MAX_VALUE;
         int ans = -1;
-        while(low <= high){
+        for(int i: bloomDay)
+        {
+            min = Math.min(i, min);
+            max = Math.max(i,max);
+        }
+        int low = min , high = max;
+        while(low <= high)
+        {
             int mid = (low + high) >> 1;
-            if(isPossible(mid, bloomDay, m,k)){
+            if(check(bloomDay,mid,k,m))
+            {
+                ans = mid;
                 high = mid - 1;
             }else{
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
-    public boolean isPossible(int mid, int[] bloomDay, int m, int k){
+    public boolean check(int[] bloomDay,int day,int k,int m)
+    {
         int count = 0;
-        int bMade = 0;
-
-        for(int i: bloomDay){
-            if(i <= mid){
+        int bouqMade= 0;
+        for(int i : bloomDay)
+        {
+            if(i <= day)
+            {
                 count++;
             }else{
-                bMade += (count / k);
-                count = 0;
+                bouqMade += (count / k);
+                count = 0; 
             }
         }
-        bMade += (count / k);
-        return (bMade >= m);
+        bouqMade += count / k;
+        return (bouqMade >= m);
     }
 }
