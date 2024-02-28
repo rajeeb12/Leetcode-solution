957107a169e537b39b0d8cdaa9a6b204dd1fd0ea
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-02-29 03:49:45 +0530

    Time: 11 ms (77.34%) | Memory: 46.3 MB (99.44%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,33 +1,39 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = 1;
-        int high = 0;
-        for(int w: weights){
+        int low = 0, high= 0;
+        for(int w: weights)
+        {
+            low = Math.max(w, low);
             high += w;
-            low =Math.max(low, w);
         }
-
-        while(low <= high){
-            int mid = (low + high) >> 1;
-            if(isPossible(mid , weights , days)){
+        int ans = -1;
+        while(low <= high)
+        {
+            int mid = (high + low ) >> 1;
+            if(getDays(weights,mid,days))
+            {
+                ans = mid;
                 high = mid - 1;
             }else{
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
-    public boolean isPossible(int mid, int[] weights, int days){
-        int countDays = 1;
-        int totalWeight = 0;
-        int n = weights.length;
-        for(int i = 0 ; i < n; i++){
-            if(totalWeight + weights[i] > mid){
-                countDays++;
-                totalWeight = weights[i];
+    public boolean getDays(int[] weights,int W,int days)
+    {
+        int sum = 0;
+        int curDays = 0;
+        for(int i: weights)
+        {
+            sum += i;
+            if(sum > W)
+            {
+                sum = i;
+                curDays++;
             }
-            else totalWeight += weights[i];
         }
-        return countDays <= days;
+        curDays++;
+        return (curDays <= days);
     }
 }
