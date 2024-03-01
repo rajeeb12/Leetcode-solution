5f800be30e046f6932294efaa39934933243c928
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-01 20:24:02 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (90.88%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,40 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
-        int n = n1 + n2;
-
-        int low = 0, high = n1;
-        int left = (n1 + n2 + 1) /2;
-
+        int n = nums1.length;
+        int m = nums2.length;
+        if(n > m)
+        {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0 , high = nums1.length;
+        int total = (n + m);
+        int half = (n + m + 1) / 2;
         while(low <= high)
         {
-            int mid1 = (high + low) >> 1; // nums1
-            int mid2 = left - mid1; // nums2
-
-            int l1 = Integer.MIN_VALUE, l2= Integer.MIN_VALUE;
-            int r1 = Integer.MAX_VALUE, r2= Integer.MAX_VALUE;
-
-            if(mid1 < n1) r1 = nums1[mid1];
-            if(mid2 < n2) r2 = nums2[mid2]; 
-
-            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            int mid1 = (low + high) >> 1;
+            int mid2 = half - mid1;
+            int left1 =(mid1 <= 0 ? Integer.MIN_VALUE : nums1[mid1 - 1]);
+            int left2 =(mid2 <= 0 ? Integer.MIN_VALUE : nums2[mid2 - 1]);
+            int right1 =(mid1 >= n ? Integer.MAX_VALUE : nums1[mid1]);
+            int right2 =(mid2 >= m ? Integer.MAX_VALUE : nums2[mid2]);
             
-            if(l1 <= r2 && l2 <= r1)
+            if(left1 <= right2 && left2 <= right1)
             {
-                if(n % 2 != 0)
-                {
-                    return Math.max(l1,l2);
-                }else{
-                    double cal = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                    return cal;
-                }
+                if(total % 2 == 1) return (double) Math.max(left1, left2);
+                int number = Math.max(left1, left2) + Math.min(right1, right2);
+                return (number / 2.0);
             }
-            else if(l1 > r2)
+            else if(left1 > right2)
             {
-                high = mid1 - 1;
+                high = mid1 -1;
             }else{
                 low = mid1 + 1;
             }
