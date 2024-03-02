8b4f432ef8a6db49a379ab3bda3f7878d128073a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-02 18:02:07 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (18.17%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -5,7 +5,21 @@ class Solution {
         {
             nums[i] *= nums[i]; 
         }
-        Arrays.sort(nums);
-        return nums;
+        int ans[] = new int[n];
+        int left = 0, right = n -1;
+        int index = n - 1;
+        while(left <= right)
+        {
+            if(nums[left] > nums[right])
+            {
+                ans[index] = nums[left];
+                left++;
+            }else{
+                ans[index] = nums[right];
+                right--;
+            }
+            index--;
+        }
+        return ans;
     }
 }
