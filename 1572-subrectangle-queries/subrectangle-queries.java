6e5e2b8d2da9f7fdee46db6fe7fb9ffe73c07ba7
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-03 10:33:16 +0530

    Time: 20 ms (100.00%) | Memory: 47.4 MB (90.84%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.java
+++ 1572-subrectangle-queries/subrectangle-queries.java
@@ -3,38 +3,16 @@ class SubrectangleQueries {
     public SubrectangleQueries(int[][] rectangle) {
         int n = rectangle.length;
         int m = rectangle[0].length;
-        arr = new int[n][m];
-        for(int i = 0 ; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                arr[i][j] = rectangle[i][j];
-            }
-        }
+        arr = rectangle;
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        if(row1 == row2)
+        
+        for(int i = row1; i <= row2; i++)
         {
-            for(int j = col1 ; j <= col2; j++)
+            for(int j = col1; j <= col2; j++)
             {
-                arr[row1][j] = newValue;
-            }
-        }else if(col1 == col2)
-        {
-            for(int j = row1 ; j <= row2; j++)
-            {
-                arr[j][col1] = newValue;
-            }
-        }
-        else
-        {
-            for(int i = row1; i <= row2; i++)
-            {
-                for(int j = col1; j <= col2; j++)
-                {
-                    arr[i][j]= newValue;
-                }
+                arr[i][j]= newValue;
             }
         }
     }
