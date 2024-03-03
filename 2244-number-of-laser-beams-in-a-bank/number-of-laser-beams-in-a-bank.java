548846a0b3a858635c67407dd1e32566fd815ebb
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-03 12:38:28 +0530

    Time: 11 ms (80.37%) | Memory: 44.8 MB (88.82%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,7 +1,9 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        ArrayList<Integer> countOfDevice = new ArrayList<>();
-
+        // optimising space
+        // storing previous count in prev named variable.
+        int prev = 0;
+        int ans = 0;
         for(String str: bank)
         {
             int count = 0;
@@ -10,13 +12,8 @@ class Solution {
                 if(ch == '1') count++;
             }
             if(count == 0) continue;
-            countOfDevice.add(count);
-        }
-        if(countOfDevice.size() <= 1) return 0;
-        int ans = 0;
-        for(int i = 0 ; i < countOfDevice.size() - 1; i++)
-        {
-            ans += (countOfDevice.get(i) * countOfDevice.get(i + 1));
+            ans += (count * prev);
+            prev = count;
         }
         return ans;
     }
