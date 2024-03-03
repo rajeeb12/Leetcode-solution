e272dac8e316719797a90a23f2681825faf9b079
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-04 00:22:31 +0530

    Time: 36 ms (43.32%) | Memory: 68 MB (34.51%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,33 +1,25 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int minTime = 0;
-        int m_Index = 0;
-        int p_Index = 0;
-        int g_Index = 0;
-
-        for(int i = 0 ; i < garbage.length ; i++){
-            int length = garbage[i].length();
-            for(char ch : garbage[i].toCharArray()){
-                if(ch == 'M'){
-                    m_Index = i;
-                }else if(ch == 'P'){
-                    p_Index = i;
-                }else if(ch == 'G'){
-                    g_Index = i;
-                }
-            }
-            minTime += length;
+        for(int i = 1; i < travel.length; i++)
+        {
+            travel[i] += travel[i - 1];
         }
-
-       int totalTime[] = new int[travel.length];
-       totalTime[0]= travel[0];
-       for(int i = 1 ; i < travel.length; i++){
-           totalTime[i] = totalTime[i - 1] + travel[i];  
-       } 
-
-       minTime += m_Index > 0 ? totalTime[m_Index - 1] : 0 ;
-       minTime += p_Index > 0 ? totalTime[p_Index - 1] : 0 ;
-       minTime += g_Index > 0 ? totalTime[g_Index - 1] : 0 ;
-       return minTime;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int sum = 0;
+        for(int i = 0; i < garbage.length; i++)
+        {
+            String str = garbage[i];
+            sum += str.length();
+            for(char ch: str.toCharArray())
+            {
+                map.put(ch, i);
+            }
+        }
+        for(Integer it: map.values())
+        {
+            if(it == 0) continue;
+            sum += travel[it - 1];
+        }
+        return sum;
     }
 }
