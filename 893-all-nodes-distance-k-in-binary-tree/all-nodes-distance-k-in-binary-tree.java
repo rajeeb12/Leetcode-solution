d0075306fddf36a932ac00c96ac62415edf1d6af
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-04 20:17:53 +0530

    Time: 15 ms (24.56%) | Memory: 42.6 MB (52.89%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -7,69 +7,77 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Pair{
+    TreeNode node;
+    int level;
+    public Pair(TreeNode _n,int _l)
+    {
+        this.node = _n;
+        this.level = _l;
+    }
+}
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-
-        // first bfs nad populating hashmap to store node and its parent
         HashMap<TreeNode, TreeNode> map = new HashMap<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root); 
+        createParentChildRel(root, map);
 
-        while(!q.isEmpty()){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(target, 0));
+        List<Integer> ans = new ArrayList<>();
+        HashSet<TreeNode> visited= new HashSet<>();
+        visited.add(target);
+
+        while(!q.isEmpty())
+        {
             int size = q.size();
+            for(int i = 0; i < size; i++)
+            {
+                Pair p = q.poll();
+                TreeNode node = p.node;
+                int curLevel = p.level;
 
-            for(int i = 0 ; i < size ; i++){
-                TreeNode node = q.poll();
-
-                if(node.left != null){ 
-                    q.add(node.left);
-                    map.put(node.left, node);
+                if(curLevel == k)
+                {
+                    ans.add(node.val);
                 }
-                if(node.right != null){
-                    q.add(node.right); 
-                    map.put(node.right, node);
+                if(node.left != null && !visited.contains(node.left))
+                {
+                    q.add(new Pair(node.left, curLevel + 1));
+                    visited.add(node.left);
                 }
-            }
-        }
-        // second bfs to traverse tree in three direction + calculating level(distance)
-        Queue<TreeNode> q1 = new LinkedList<>();
-
-        HashSet<TreeNode> set= new HashSet<>(); // to keep track of visited nodes
-        int distance = 0;
-        q1.add(target);
-        set.add(target);
-
-        while(!q1.isEmpty()){
-            int size = q1.size();
-
-            if(distance == k){
-                break;
-            }
-            for(int i= 0 ; i < size ; i++){
-                TreeNode node = q1.poll();
-                TreeNode parentNode = map.get(node);
-                if(node.left != null && !set.contains(node.left)){
-                    q1.add(node.left);
-                    set.add(node.left);
-                } 
-                if(node.right != null && !set.contains(node.right)){    
-                    q1.add(node.right);
-                    set.add(node.right);
-                } 
-                if(parentNode != null && !set.contains(parentNode)){
-                    set.add(parentNode);
-                    q1.add(parentNode);
+                if(node.right != null && !visited.contains(node.right))
+                {
+                    q.add(new Pair(node.right, curLevel + 1));
+                    visited.add(node.right);
+                }
+                TreeNode parent = map.get(node);
+                if(parent != null && !visited.contains(parent))
+                {
+                    q.add(new Pair(parent, curLevel + 1));
+                    visited.add(parent);
                 }
             }
-            distance++;
-        }
-        int size = q1.size();
-        for(int i = 0 ; i < size ; i++){
-            TreeNode node = q1.poll();
-            ans.add(node.val);
         }
         return ans;
+    }
+    public void createParentChildRel(TreeNode root, HashMap<TreeNode, TreeNode> map)
+    {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        map.put(root, null);
+        while(!q.isEmpty())
+        {
+            TreeNode node = q.poll();
+            if(node.left != null)
+            {
+                q.add(node.left);
+                map.put(node.left, node);
+            }
+            if(node.right != null)
+            {
+                q.add(node.right);
+                map.put(node.right, node);
+            }
+        }
     }
 }
