58fe3e234d09ecc0d0077837dc00605b2bf42980
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-03-18 19:20:14 +0530

    Time: 54 ms (65.36%) | Memory: 68.6 MB (77.37%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,20 +1,20 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points ,(a,b)-> Integer.compare(a[1], b[1]));
-        int start = points[0][0];
+        if(points.length == 0) return 0;
+
+        Arrays.sort(points,(a,b) -> Integer.compare(a[1], b[1]));
+        int countArrows = 1;
         int end = points[0][1];
-        int n = points.length;
-        
-        int arrow = 1;
-        for(int i = 1 ; i < n ; i++)
+
+        for(int i = 1; i < points.length; i++)
         {
-            if(end < points[i][0])
+            if(end >= points[i][0])
             {
-                arrow++;
-                start = points[i][0];
-                end = points[i][1];
+                continue;
             }
+            countArrows++;
+            end = points[i][1];
         }
-        return arrow;
+        return countArrows;
     }
 }
