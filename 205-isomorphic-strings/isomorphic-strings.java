b44e3d0dcb870e2ec741b3c2ecf7ed98bfc6dbac
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-04-02 15:36:07 +0530

    Time: 7 ms (89.77%) | Memory: 42.5 MB (26.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,38 +1,13 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-       if(s.length() != t.length()) return false;
-       HashMap<Character, List<Integer>> map1 = new HashMap<>();
-       HashMap<Character, List<Integer>> map2 = new HashMap<>();
-       for(int i = 0; i < t.length(); i++)
+       int map1[] = new int[256];
+       int map2[] = new int[256];
+       for(int i = 0 ; i < s.length(); i++)
        {
-        char c = s.charAt(i);
-        if(!map1.containsKey(c))
-        {
-            map1.put(c, new ArrayList<>());
-        }
-        char d = t.charAt(i);
-        if(!map2.containsKey(d))
-        {
-            map2.put(d, new ArrayList<>());
-        }
-        map1.get(c).add(d -'a');
-        map2.get(d).add(c - 'a');
-       }
-       for(Map.Entry<Character,List<Integer>> it: map1.entrySet())
-       {
-        List<Integer> ls = it.getValue();
-        for(int i = 0; i < ls.size() - 1; i++)
-        {
-            if(ls.get(i) != ls.get(i + 1)) return false;
-        }
-       }
-       for(Map.Entry<Character,List<Integer>> it: map2.entrySet())
-       {
-        List<Integer> ls = it.getValue();
-        for(int i = 0; i < ls.size() - 1; i++)
-        {
-            if(ls.get(i) != ls.get(i + 1)) return false;
-        }
+        if(map1[s.charAt(i)] != map2[t.charAt(i)]) return false;
+
+        map1[s.charAt(i)] = i + 1;
+        map2[t.charAt(i)] = i + 1;
        }
        return true;
 
