ed53395010d30f2b69ea1528c1bb96b640a2639d
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-04-04 04:52:06 +0530

    Time: 32 ms (85.00%) | Memory: 59.9 MB (13.45%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
@@ -1,27 +1,29 @@
 class Solution {
     public int countNicePairs(int[] nums) {
+        HashMap<Long,Integer> map = new HashMap<>();
         int mod = (int)1e9 + 7;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int n = nums.length;
-        int count = 0; 
-        for(int i = 0 ; i < n; i++){
-            int reverse = rev(nums[i]);
-            int cal= (reverse - nums[i]);
-
-            count = ((count + map.getOrDefault(cal, 0)) % mod);
-            map.put(cal, map.getOrDefault(cal,0) + 1);
-
+        for(int i : nums)
+        {
+            long reverse = rev(i);
+            long sum = i - reverse;
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return count % mod;
+        long ans = 0;
+        for(long i: map.values())
+        {
+            long temp = (i * (i - 1)) / 2;
+            ans = (ans + temp) % mod;
+        }
+        return (int)ans;
     }
-    public int rev(int x){
-        int res = 0;
-
-        while(x != 0){
-            int rem = x % 10;
-            res = res * 10 + rem;
-            x /= 10;
+    public long rev(int n)
+    {
+        long sum = 0;
+        while(n > 0){
+            int rem = n % 10;
+            sum = sum * 10 + rem;
+            n /= 10;
         }
-        return res;
+        return sum;
     }
 }
