559152ca0e4352142c0b7b5aeab36cc4208c7523
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-01 17:36:51 +0530

    Time: 16 ms (85.41%) | Memory: 56.9 MB (40.76%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -2,51 +2,52 @@ class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int inDegree[] = new int[n];
-        for(int i = 0 ;i < n ; i++)
+        for(int i =0 ; i < n; i++)
         {
             adj.add(new ArrayList<>());
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        if(n == 1){
-            ans.add(0);
-            return ans;
-        }
-
         for(int e[]: edges)
         {
             int u = e[0];
             int v = e[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
-            inDegree[v]++;
             inDegree[u]++;
+            inDegree[v]++;
         }
+        List<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i < n; i++)
+        if(n == 1)
         {
-            if(inDegree[i] == 1) q.add(i);
+            ans.add(0);
+            return ans;
         }
-
-        while(n > 2)
+        for(int i = 0; i < n; i++)
         {
+            if(inDegree[i] == 1)
+            {
+                q.add(i);
+            }
+        }
+        while(n > 2){
             int size = q.size();
             n -= size;
 
-            for(int i = 0 ; i < size ; i++)
+            for(int i = 0; i < size; i++)
             {
-                int node = q.poll();
-                for(int adjNode: adj.get(node))
+                int node =q.poll();
+                for(int adjnode: adj.get(node))
                 {
-                    inDegree[adjNode]--;
-                    if(inDegree[adjNode] == 1)
+                    inDegree[adjnode]--;
+                    if(inDegree[adjnode] == 1)
                     {
-                        q.add(adjNode);
+                        q.add(adjnode);
                     }
                 }
             }
         }
-        ans.addAll(q);
-        
-        return ans;
-    }  
+        while(!q.isEmpty()) ans.add(q.poll());
+        return (ans);
+
+    }
 }
