5b7cc8f22e2230f62a624f45c303eada80a405a2
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-03 20:46:35 +0530

    Time: 1 ms (39.50%) | Memory: 44.8 MB (41.93%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,27 +1,24 @@
 class Pair{
     int i;
     int j;
-    int color;
-    public Pair(int _i,int _j,int _c)
+    public Pair(int _i,int _j)
     {
         this.i = _i;
         this.j = _j;
-        this.color = _c;
     }
 }
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc,image[sr][sc]));
+        q.add(new Pair(sr,sc));
         
+        int curColor = image[sr][sc];
         int n = image.length;
         int m = image[0].length;
         int di[]={1,-1,0,0};
         int dj[]={0,0,1,-1};
         int visited[][] = new int[n][m];
-
-        visited[sr][sc] = 1;
 
         while(!q.isEmpty())
         {
@@ -31,7 +28,6 @@ class Solution {
                 Pair p = q.poll();
                 int x = p.i;
                 int y = p.j;
-                int c = p.color;
 
                 image[x][y] = color;
 
@@ -39,10 +35,14 @@ class Solution {
                 {
                     int ni = x + di[k];
                     int nj = y + dj[k];
-                    if(ni >= 0 && nj >= 0 && ni < n && nj < m && image[ni][nj] == c && visited[ni][nj] == 0)
-                    {
-                        visited[ni][nj] = 1;
-                        q.add(new Pair(ni,nj, c));
+                    if(ni >= 0 && nj >= 0 && ni < n && nj < m){
+                        if(image[ni][nj] == color) continue;
+
+                        if(image[ni][nj] == curColor)
+                        {
+                            visited[ni][nj] = 1;
+                            q.add(new Pair(ni,nj));
+                        }
                     }
                 }
             }
