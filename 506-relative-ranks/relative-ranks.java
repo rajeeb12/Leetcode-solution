b5f6dd11312273000d4d4d6e6b12dcc4f1ab3a82
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-08 17:27:40 +0530

    Time: 7 ms (93.93%) | Memory: 45.3 MB (92.42%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,19 +1,27 @@
+class Pair{
+    int val;
+    int index;
+    public Pair(int _v,int _i)
+    {
+        this.val = _v;
+        this.index = _i;
+    }
+}
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> Integer.compare(y , x));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> Integer.compare(y.val , x.val));
 
         for(int i = 0; i < score.length; i++)
         {
-            map.put(score[i], i);
-            pq.add(score[i]);
+            pq.add(new Pair(score[i] , i));
         } 
         int n = score.length;
         String ans[] = new String[n];
         int count = 1;
         while(!pq.isEmpty())
         {
-            int index = map.get(pq.poll());
+            Pair p = pq.poll();
+            int index = p.index;
             if(count <= 3){
                 if(count == 1){
                     ans[index] = "Gold Medal";
