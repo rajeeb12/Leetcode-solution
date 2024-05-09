de491ebc7eb11c18e975241d42f29ce1ebf0010e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-09 20:32:35 +0530

    Time: 26 ms (17.19%) | Memory: 75.5 MB (15.96%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,27 +1,50 @@
 class Solution {
     public int findCenter(int[][] edges) {
+        int n = 0;
+        for(int i[]: edges){
+            n = Math.max(n, Math.max(i[0], i[1]));
+        }
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int n = edges.length + 1; 
+        int inDegree[] = new int[n + 1];
 
-        for(int i = 0 ; i <= n; i++)
+        for(int i = 0; i <= n; i++)
         {
             adj.add(new ArrayList<>());
         }
 
-        for(int e[]: edges)
+        for(int e[] : edges)
         {
             int u = e[0];
             int v = e[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
+            inDegree[u]++;
+            inDegree[v]++; 
         }
-        int ans = 0;
-        for(int i = 1 ; i <= n ; i++)
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i <= n; i++)
         {
-            if(adj.get(i).size() == n-1){
-                return i;
-            }
+            if(inDegree[i] == 1) q.add(i);
         }
-        return -1;
+        int count = q.size();
+        while(count < n)
+        {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++)
+            {
+                int child = q.poll();
+                for(int parent: adj.get(child))
+                {
+                    inDegree[parent]--;
+                    if(inDegree[parent] == 1)
+                    {
+                        q.add(parent);
+                    }
+                }
+            }
+            count += q.size();
+        }
+        return q.poll();
     }
 }
