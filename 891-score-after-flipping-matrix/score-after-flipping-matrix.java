68b7204aa05e56c8f1019ba88a1769b34e34fd73
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-13 19:16:06 +0530

    Time: 1 ms (45.15%) | Memory: 41.1 MB (71.82%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int matrixScore(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i = 0 ; i < n; i++)
+        {
+            if(grid[i][0] == 0)
+            {
+                toogle(i, grid);
+            }
+        }
+        for(int j = 0; j < m; j++)
+        {
+            int count0 = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(grid[i][j] == 0) count0++;
+            }
+            //System.out.print(count0);
+            int count1 = n - count0;
+            if(count0 > count1)
+            {
+                toogleCol(j,grid);
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            int num = 0;
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1) num |= (1 << m - j - 1);
+            }
+            //System.out.print(num);
+            sum += num;
+        }
+        return sum;
+    }
+    public void toogle(int row,int[][] grid)
+    {
+        for(int i = 0; i < grid.length; i++)
+        {
+            if(i != row) continue;
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] == 1) grid[i][j] = 0;
+                else grid[i][j] = 1;
+            }
+        }
+    }
+    public void toogleCol(int col,int[][] grid)
+    {
+        for(int j = 0; j < grid[0].length; j++)
+        {
+            if(j != col) continue;
+            for(int i = 0; i < grid.length; i++)
+            {
+                if(grid[i][j] == 1) grid[i][j] = 0;
+                else grid[i][j] = 1;
+            }
+        }
+    }
+}
