759d14e4110da4e2248fc7adf513ed37e9741743
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-13 20:46:12 +0530

    Time: 2 ms (99.88%) | Memory: 46.1 MB (6.63%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,26 +1,34 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int ans[] = new int[n];
-        Arrays.fill(ans, 1);
+        //optimised
+        int candy = n;
 
-        for(int i = 1; i < n ; i++)
+        int i = 1;
+        while(i < n)
         {
-            if(ratings[i] > ratings[i - 1] )
-            {
-                ans[i] = ans[i - 1] + 1;
+            if(ratings[i] == ratings[i - 1]){
+                i++;
+                continue;
             }
-        }  
-        for(int i = n - 2; i >= 0; i--)
-        {
-            if(ratings[i] > ratings[i + 1] && ans[i] <= ans[i + 1])
-            {
-                ans[i] = Math.max(ans[i] , ans[i + 1]) + 1;
+            int peak = 0;
+
+            while(i < n && ratings[i] > ratings[i - 1]){
+                ++peak;
+                candy += peak;
+                i++;
             }
-        }  
-        
-        int sum = 0;
-        for(int candy: ans) sum += candy;
-        return sum;
+
+            if(i == n) return candy;
+
+            int dip = 0;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                ++dip;
+                candy += dip;
+                i++;
+            }
+            candy -= Math.min(dip, peak);
+        }
+        return candy ;
     }
 }
