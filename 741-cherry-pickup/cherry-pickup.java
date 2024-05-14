9ec612b31aabb4b93a623e2e41367365fbf5cd8d
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-15 02:11:53 +0530

    Time: 12 ms (86.16%) | Memory: 44.6 MB (75.89%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,28 +1,26 @@
 class Solution {
-    int dp[][][][];
+    int dp[][][];
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        dp = new int[n + 1][n + 1][n + 1][n + 1];
+        dp = new int[n + 1][n + 1][n + 1];
 
-        for(int i[][][]: dp){
-            for(int j[][]: i){
-                for(int k[] : j)
-                {
-                    Arrays.fill(k, - 1);
-                }
+        for(int i[][]: dp){
+            for(int j[]: i){
+                Arrays.fill(j, - 1);
             }
         }
-        int ans = solve(0,0,0,0,grid);
+        int ans = solve(0,0,0,grid);
         return (ans < 0 ? 0 : ans);
     }
-    public int solve(int r1,int c1,int r2,int c2,int[][] grid)
+    public int solve(int r1,int c1,int r2,int[][] grid)
     {
+        int c2 = r1 + c1 - r2;
         int n = grid.length;
         if(r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1) return Integer.MIN_VALUE;
         
         if(r1 == n - 1 && c1 == n - 1) return grid[r1][c1];
 
-        if(dp[r1][c1][r2][c2] != -1) return dp[r1][c1][r2][c2];
+        if(dp[r1][c1][r2] != -1) return dp[r1][c1][r2];
         
         int cherry = 0;
         if(r1 == r2 && c1 == c2)
@@ -32,14 +30,14 @@ class Solution {
             cherry = grid[r1][c1] + grid[r2][c2];
         }
 
-        int f1 = solve(r1 , c1 + 1 , r2 , c2 + 1 , grid);
-        int f2 = solve(r1 + 1 , c1 , r2 , c2 + 1 , grid);
-        int f3 = solve(r1 + 1 , c1 , r2 + 1 , c2 , grid);
-        int f4 = solve(r1 , c1 + 1 , r2 + 1 , c2 , grid);
+        int f1 = solve(r1 , c1 + 1 , r2 , grid);
+        int f2 = solve(r1 + 1 , c1 , r2 , grid);
+        int f3 = solve(r1 + 1 , c1 , r2 + 1, grid);
+        int f4 = solve(r1 , c1 + 1 , r2 + 1, grid);
 
         cherry += Math.max(f1, Math.max(f2,Math.max(f3, f4)));
 
-        return dp[r1][c1][r2][c2] =  cherry;
+        return dp[r1][c1][r2] =  cherry;
         
     }
 }
