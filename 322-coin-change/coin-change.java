57a310e8c3373d33c673cc6c707cab002561c21a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-15 03:22:59 +0530

    Time: 38 ms (17.16%) | Memory: 45.1 MB (19.44%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,32 +3,27 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         dp = new int[n + 1][amount + 1];
-        for(int i[]: dp)
-        {
-            Arrays.fill(i, -1);
-        }
-        int ans = solve(0, coins, amount);
+        for(int i[]: dp) Arrays.fill(i, -1);
+
+        int ans = solve(0,coins,amount);
         return (ans == (int)1e9 ? -1 : ans);
     }
-    public int solve(int index, int[] coins,int amt)
+    public int solve(int index,int[] coins,int amount)
     {
+        if(amount < 0) return (int)1e9;
+
         if(index == coins.length)
         {
-            if(amt == 0)
-            {
-                return 0;
-            }
-            return (int)1e9;
+            return (amount == 0 ? 0 : (int)1e9);
         }
-        
-        if(dp[index][amt] != -1) return dp[index][amt];
 
-        int notPick = solve(index + 1,coins, amt);
+        if(dp[index][amount] != -1) return dp[index][amount];
+
+        int notPick = solve(index + 1, coins, amount);
         int pick = (int)1e9;
-        if(coins[index] <= amt)
-        {
-            pick = 1 + solve(index , coins , amt - coins[index]);
-        }
-        return dp[index][amt] = Math.min(notPick , pick);
+
+        if(coins[index] <= amount) pick = 1 + solve(index, coins, amount - coins[index]);
+
+        return dp[index][amount] = Math.min(notPick , pick);
     }
 }
