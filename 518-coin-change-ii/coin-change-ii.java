0b90bdbc23662f9016470930f42105d52ef00b85
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-20 23:45:04 +0530

    Time: 7 ms (51.54%) | Memory: 47.1 MB (57.64%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,25 +1,43 @@
 class Solution {
     int dp[][];
-    public int change(int amount, int[] coins) {
-        dp = new int[coins.length + 1][amount + 1];
-        for(int i[]: dp) Arrays.fill(i, -1);
-
-        return solve(0,coins,amount);
-    }
-    public int solve(int index,int[] coins,int amount)
-    {
-        if(amount == 0) return 1;
-        if(index == coins.length) return 0;
+    public int change(int a, int[] coins) {
+        int n = coins.length;
+        dp = new int[coins.length + 1][a + 1];
         
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        int notpick = solve(index + 1, coins, amount);
-        int pick = 0;
-
-        if(coins[index] <= amount){
-            pick = solve(index , coins, amount - coins[index]);
+        for(int i = 0; i < n; i++)
+        {
+            dp[i][0] = 1;
         }
+        for(int index = n - 1; index >= 0; index--){
+            for(int amount = 1; amount <= a; amount++){
 
-        return dp[index][amount] = notpick + pick;
+                int notpick = dp[index + 1][amount];
+                int pick = 0;
+
+                if(coins[index] <= amount){
+                    pick = dp[index][amount - coins[index]];
+                }
+
+                dp[index][amount] = notpick + pick;
+            }
+        }
+        return dp[0][a];
+        //return solve(0,coins,amount);
     }
+    // public int solve(int index,int[] coins,int amount)
+    // {
+    //     if(amount == 0) return 1;
+    //     if(index == coins.length) return 0;
+        
+    //     if(dp[index][amount] != -1) return dp[index][amount];
+
+    //     int notpick = solve(index + 1, coins, amount);
+    //     int pick = 0;
+
+    //     if(coins[index] <= amount){
+    //         pick = solve(index , coins, amount - coins[index]);
+    //     }
+
+    //     return dp[index][amount] = notpick + pick;
+    // }
 }
