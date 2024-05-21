f7363f07f80495f4fbfb9b1d7766a6b8962a9b4e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-22 01:32:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (26.29%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -3,11 +3,10 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
 
-        for(int i = 0 ; i <= 32; i++)
-        {
-            int bit = n & 1;
-            count += (bit == 1 ? 1 : 0);
-            n >>>= 1;
+        while(n != 0){
+            int bit = n & ~(n - 1);
+            if(bit == 1) count++;
+            n = n >> 1;
         }
         return count;
     }
