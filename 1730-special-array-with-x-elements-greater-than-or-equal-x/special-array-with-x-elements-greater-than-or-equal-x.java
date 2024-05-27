adab1d370e6de4268acdcbda564c7728bc15d93f
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-27 20:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (95.39%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,31 +1,21 @@
 class Solution {
     public int specialArray(int[] nums) {
-        Arrays.sort(nums);
-        int low = 0 , high = 1000;
-        while(low <= high){ 
-            int mid = (low + high) >> 1;
-            int count = getCount(mid, nums);
-            if(mid == count){
-                return mid;
-            }else if(count > mid){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
+        int n = nums.length;
+        int arr[] = new int[n + 1];
+        int csum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] >= n){
+                arr[n]++;
             }
+            else{
+                arr[nums[i]]++;
+            }
+        }
+        for(int i = n ; i >= 0; i--){
+            csum += arr[i];
+            if(csum == i) return i;
         }
         return -1;
-    }
-    public int getCount(int num,int nums[]){
-        int count = 0;
-        int low = 0, high = nums.length - 1;
-        while(low <= high){
-            int mid = (low + high) >> 1;
-            if(nums[mid] >= num){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        return nums.length - low;    
     }
 }
