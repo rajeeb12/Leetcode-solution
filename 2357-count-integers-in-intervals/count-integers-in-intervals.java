09a36e9c9bd57d84027f58054355ea60f4281538
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-28 03:15:40 +0530

    Time: 90 ms (77.53%) | Memory: 86.1 MB (80.27%) - LeetSync

---
+++ 2357-count-integers-in-intervals/count-integers-in-intervals.java
@@ -0,0 +1,38 @@
+class CountIntervals {
+    TreeMap<Integer,Integer> map;
+    int count;
+    public CountIntervals() {
+        map = new TreeMap<>();
+        count = 0;
+    }
+    
+    public void add(int left, int right) {
+        int l = left;
+        int r = right;
+        
+        while(!(map.floorKey(r) == null) && (map.get(map.floorKey(r )) >= l))
+        {
+            int minL= map.floorKey(r);
+            int maxR = map.get(minL);
+
+            count -= (maxR - minL + 1);
+            map.remove(minL);
+
+            l = Math.min(l, minL);
+            r = Math.max(r, maxR);
+        }
+        map.put(l, r);
+        count += (r - l + 1);
+    }
+    
+    public int count() {
+        return count;
+    }
+}
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * CountIntervals obj = new CountIntervals();
+ * obj.add(left,right);
+ * int param_2 = obj.count();
+ */
