55527c1005b8f5d30b5247bd13e24bff2fb587af
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-05-29 16:14:34 +0530

    Time: 180 ms (12.82%) | Memory: 64.3 MB (6.25%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,19 +1,25 @@
 class Solution {
     public int countPrimes(int n) {
-        int isPrime[]= new int[n + 1];
-
-        for(int i = 2 ; i*i < n ; i++)
-        {
-            for(int j = i*i ; j < n; j += i)
-            { 
-                isPrime[j] = 1;
-            }
-        }   
+        if(n == 0 || n == 1 ) return 0;
+        int prime[] = seive(n);
         int count = 0;
-        for(int i = 2; i < n; i++)
-        {
-            count += (isPrime[i] == 0 ? 1 : 0);
+        for(int i = 2; i < n; i++){
+            if(prime[i] == 1) count++;
         }
         return count;
+    }
+    public int[] seive(int n)
+    {
+        int arr[] = new int[n + 1];
+        Arrays.fill(arr, 1);
+
+        for(int i = 2; i <= n; i++)
+        {
+            if(arr[i] == 0) continue;
+            for(int j = 2 * i ; j <= n; j +=i){
+                arr[j] = 0;
+            }
+        }
+        return arr;
     }
 }
