bbad3447b923ec1e85fc6ca559c7f4d8992e8399
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-06-17 03:26:53 +0530

    Time: 50 ms (5.14%) | Memory: 51.3 MB (10.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,44 +1,32 @@
 class Solution {
     int dp[][];
-    public int longestCommonSubsequence(String str1, String str2) {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        dp = new int[n + 1][m + 1];
+        for(int i[]: dp) Arrays.fill(i, -1);
 
-        int n1 = str1.length();
-        int n2 = str2.length();
-        dp = new int[n1 + 1][n2 + 1];
-
-        for(int i1 = 1 ; i1 <= n1; i1++)
-        {
-            for(int i2 = 1 ; i2 <= n2; i2++)
-            {
-                int pick = 0;
-                int notPick = 0;
-                if(str1.charAt(i1 - 1) == str2.charAt(i2 - 1))
-                {
-                    pick = 1 + dp[i1 - 1][i2 - 1];
-                }
-                notPick = Math.max(dp[i1 - 1][i2] , dp[i1][i2 - 1]);
-
-                dp[i1][i2] = Math.max(pick, notPick);
+        int ans = 0;
+        for(int i = n - 1; i >=0 ; i--){
+            for(int j = m - 1; j >= 0; j--){
+                ans = Math.max(ans, solve(i,j, text1 , text2));
             }
         }
-
-        //return solve(text1, text2, n1 - 1, n2 -1);
-        return dp[n1][n2];    
+        return ans;
     }
-    public int solve(String str1, String str2,int i1,int i2)
-    {
-        if(i1 < 0 || i2 < 0) return 0;
+    public int solve(int i,int j,String s1,String s2){
+        if(i < 0 || j < 0) return 0;
         
-        if(dp[i1][i2] != -1) return dp[i1][i2];
-
-        int pick = 0;
-        int notPick = 0;
-        if(str1.charAt(i1) == str2.charAt(i2))
-        {
-            pick = 1 + solve(str1, str2, i1 - 1, i2 - 1);
+        if(dp[i][j] != -1) return dp[i][j];
+    
+        int ans = 0;
+        if(s1.charAt(i) == s2.charAt(j)){
+            ans = 1 + solve(i - 1, j - 1, s1, s2); 
+        }else{
+            int a = solve(i - 1, j, s1 ,s2);
+            int b = solve(i , j - 1, s1, s2);
+            ans = Math.max(ans, Math.max(a, b));
         }
-        notPick = Math.max(solve(str1, str2, i1 - 1, i2) , solve(str1, str2, i1, i2 -1));
-
-        return dp[i1][i2] = Math.max(pick, notPick);
+        return dp[i][j] = ans;
     }
 }
