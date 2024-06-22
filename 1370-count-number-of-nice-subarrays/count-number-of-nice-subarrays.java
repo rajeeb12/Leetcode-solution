b5d25123a2358e02ce91e10414b6fb2c4e5d4406
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-06-22 15:05:01 +0530

    Time: 10 ms (80.88%) | Memory: 54.7 MB (82.00%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,30 +1,22 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return getCountOfSubarrayWithK(nums, k) - getCountOfSubarrayWithK(nums, k - 1);
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = nums[i] % 2;
+        }
+        return solve(nums, k) - solve(nums, k - 1);
     }
-    public int getCountOfSubarrayWithK(int[] nums,int k)
-    {
-        int count = 0;
-        int res = 0;
-        int i=0;
-        for(int j = 0 ; j < nums.length; j++)
-        {
-            if(nums[j] % 2 != 0)
-            {
-                count++;
-            }
-
-            while(count > k)
-            {
-                if(nums[i] % 2 != 0)
-                {
-                    count--;
-                }
+    public int solve(int nums[],int k){
+        int sum = 0;
+        int i = 0;
+        int ans = 0;
+        for(int j = 0; j < nums.length; j++){
+            sum += nums[j];
+            while(sum > k && i <=j){
+                sum -= nums[i];
                 i++;
             }
-
-            res +=  (j - i + 1);
+            ans += (j - i + 1);
         }
-        return res;
+        return ans;
     }
 }
