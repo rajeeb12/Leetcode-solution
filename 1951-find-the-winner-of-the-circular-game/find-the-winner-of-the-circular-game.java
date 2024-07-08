ed41275de671f654ae206863b8a220571d01e856
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-07-09 02:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (82.87%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,41 +1,9 @@
-class Node{
-    int num;
-    Node next;
-    public Node(int _num){
-        this.num = _num;
-        next = null;
-    }
-}
 class Solution {
     public int findTheWinner(int n, int k) {
-        if(k == 1) return n;
-
-        Node head= new Node(1);
-        Node cur = head;
-        for(int i = 2; i <= n; i++){
-            Node node = new Node(i);
-            cur.next = node;
-            cur = node; 
-        }
-        cur.next = head;
-        return solve(head, k);
+        return solve(n,k) + 1;
     }
-    public int solve(Node head, int k){
-        Node cur = head;
-        int ans = 0;
-        while(cur.next != cur)
-        {
-            Node prev = null;
-            for(int i = 1; i < k; i++){
-                prev = cur;
-                cur = cur.next;
-            }
-            if(prev == cur){
-                cur = cur.next;
-            }
-            prev.next = cur.next;
-            cur = cur.next;
-        }
-        return cur.num;
+    public int solve(int n,int k){
+        if(n == 1) return 0;
+        return (solve(n - 1, k) + k) % n;
     }
 }
