9b3609084246e4d2a8778ca4b4fbdeb7adeb96ae
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-07-12 19:07:44 +0530

    Time: 86 ms (64.65%) | Memory: 45.3 MB (88.89%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,36 +1,36 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        Stack<Character> st = new Stack<>();
-        char first = (x > y ? 'a' : 'b');
-        char second = (x > y ? 'b' : 'a');
-        int min = Math.min(x, y);
+        int n = s.length();
         int max = Math.max(x,y);
+        int min = Math.min(x,y);
+        String maxstr = (x > y ? "ab" : "ba");
+        String minstr = (x > y ? "ba" : "ab");
         int ans = 0;
-        for(char c: s.toCharArray())
-        {
-            if(c == second)
-            {
-                if(!st.isEmpty() && st.peek() == first)
-                {
-                    ans += max;
-                    st.pop();
-                }else{
-                    st.add(c);
-                }
-            }else{
-                st.add(c);
-            }
-        }
-        Stack<Character> st2 = new Stack<>();
-        while(!st.isEmpty()){
-            char c = st.pop();
-            if(!st2.isEmpty() && st2.peek() == first && c == second){
-                ans += min;
-                st2.pop();
-            }else{
-                st2.add(c);
-            }
-        }
+
+        String s1 = removeString(s, maxstr);
+        int temp1 = (n - s1.length()) / 2;
+        ans = temp1 * max;
+
+        String s2 = removeString(s1, minstr);
+        int temp2 = (s1.length() - s2.length()) / 2;
+        ans += temp2 * min;
+        
         return ans;
+    }
+    public String removeString(String s,String maxstr){
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+
+        for(int j = 0; j < s.length(); j++){
+            str.append(s.charAt(j));
+            i++;
+
+            if(i >= 2 && str.charAt(i - 2) == maxstr.charAt(0) && str.charAt(i - 1) == maxstr.charAt(1)){
+                str.delete(i - 2, i);
+                i -= 2;
+            }
+        }
+        System.out.print(str+" ");
+        return str.toString();
     }
 }
