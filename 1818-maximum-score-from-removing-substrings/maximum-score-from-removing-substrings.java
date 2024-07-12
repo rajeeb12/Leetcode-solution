397afc3c9f614f761939b364bfc69083bc991683
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-07-12 18:11:52 +0530

    Time: 134 ms (58.59%) | Memory: 46.3 MB (53.54%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -3,6 +3,8 @@ class Solution {
         Stack<Character> st = new Stack<>();
         char first = (x > y ? 'a' : 'b');
         char second = (x > y ? 'b' : 'a');
+        int min = Math.min(x, y);
+        int max = Math.max(x,y);
         int ans = 0;
         for(char c: s.toCharArray())
         {
@@ -10,7 +12,7 @@ class Solution {
             {
                 if(!st.isEmpty() && st.peek() == first)
                 {
-                    ans += (x > y ? x : y);
+                    ans += max;
                     st.pop();
                 }else{
                     st.add(c);
@@ -19,33 +21,14 @@ class Solution {
                 st.add(c);
             }
         }
-        String str = "";
+        Stack<Character> st2 = new Stack<>();
         while(!st.isEmpty()){
             char c = st.pop();
-            str = c + str;
-        }
-
-        return ans + solve(str, (x > y ? y : x), x, y);
-    }
-    public int solve(String s, int points,int x,int y)
-    {
-        Stack<Character> st = new Stack<>();
-        int ans = 0;
-        char first = (x < y ? 'a' : 'b');
-        char second = (x < y ? 'b' : 'a');
-        for(char c: s.toCharArray())
-        {
-            if(c == second)
-            {
-                if(!st.isEmpty() && st.peek() == first)
-                {
-                    ans += points;
-                    st.pop();
-                }else{
-                    st.add(c);
-                }
+            if(!st2.isEmpty() && st2.peek() == first && c == second){
+                ans += min;
+                st2.pop();
             }else{
-                st.add(c);
+                st2.add(c);
             }
         }
         return ans;
