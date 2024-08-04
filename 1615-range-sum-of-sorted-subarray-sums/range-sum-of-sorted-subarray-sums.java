ffee9555944168720949a5f22d2f84d38ff4693a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-08-04 15:31:55 +0530

    Time: 207 ms (11.26%) | Memory: 45.3 MB (82.43%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,20 +1,37 @@
+class Pair{
+    int num;
+    int index;
+    public Pair(int _n,int _i){
+        this.num = _n;
+        this.index = _i;
+    }
+}
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> temp = new ArrayList<>();
-        for(int i = 0; i < n; i++)
-        {
-            int sum = 0;
-            for(int j = i; j < n; j++){
-                sum += nums[j];
-                temp.add(sum);
-            }
-        }
-        Collections.sort(temp);
-        int sum = 0; 
         int mod = (int)1e9 + 7;
-        for(int i = left - 1 ; i < right; i++){
-            sum = (sum + temp.get(i)) % mod;
+        PriorityQueue<Pair> pq = new PriorityQueue<>( (a, b) -> a.num - b.num);
+        for(int i = 0; i < n; i++){
+            pq.add(new Pair(nums[i] , i));
         }
-        return sum;
+        int sum = 0;
+        left--;
+        right--;
+        int i = 0;
+        while(!pq.isEmpty())
+        {
+            Pair temp = pq.poll();
+            int number = temp.num;
+            int index = temp.index;
+            
+            if(i >= left && i <= right)
+            {
+                sum = (sum + number) % mod;
+            }
+            if(index + 1 < n){
+                pq.add(new Pair(number + nums[index + 1] , index + 1));
+            }
+            i++;
+        }
+        return sum % mod;
     }
 }
