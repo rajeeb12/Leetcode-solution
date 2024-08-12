af9da5654b0b3b671a6c3b8168ccf3a5d4a3f1be
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-08-12 19:30:55 +0530

    Time: 37 ms (39.74%) | Memory: 44.6 MB (21.80%) - LeetSync

--- 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.java
+++ 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.java
@@ -1,42 +1,65 @@
 class Solution {
+    int dp[];
     public int minDays(int[][] grid) {
-        if (countIslands(grid) != 1) return 0;
+        List<int[]> arr = new ArrayList<>();
+        int sum = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    arr.add(new int[]{i,j});
+                    sum += 1;
+                }
+            }
+        }    
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    if (countIslands(grid) != 1) return 1;
-                    grid[i][j] = 1;
+        int visited[][] = new int[n][m];
+        int count = 0;
+        for(int k = 0; k < n; k++){
+            for(int l = 0; l < m; l++){
+                if(grid[k][l] == 1 && visited[k][l] == 0){
+                    dfs(k,l,grid,visited,n,m);
+                    count++;
                 }
             }
         }
+        if(count > 1 || count == 0) return 0;
 
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 0) continue;
+
+                grid[i][j] = 0;
+                int visited2[][] = new int[n][m];
+                int count2 = 0;
+                for(int k = 0; k < n; k++){
+                    for(int l = 0; l < m; l++){
+                        if(grid[k][l] == 1 && visited2[k][l] == 0){
+                            dfs(k,l,grid,visited2,n,m);
+                            count2++;
+                        }
+                    }
+                }
+                if(count2 > 1 || count2 == 0){
+                    return 1;
+                }
+                grid[i][j] = 1;
+            }
+        }
         return 2;
     }
-
-    private int countIslands(int[][] grid) {
-        boolean[][] seen = new boolean[grid.length][grid[0].length];
-        int islands = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1 && !seen[i][j]) {
-                    islands++;
-                    dfs(grid, i, j, seen);
-                }
-            }
-        }
-        return islands;
-    }
-
-    private void dfs(int[][] grid, int r, int c, boolean[][] seen) {
-        seen[r][c] = true;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int[] dir : directions) {
-            int nr = r + dir[0], nc = c + dir[1];
-            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1 && !seen[nr][nc]) {
-                dfs(grid, nr, nc, seen);
+    public void dfs(int i,int j,int grid[][],int[][] visited,int n,int m){
+        visited[i][j] = 1;
+        int di[]= {1,-1,0,0};
+        int dj[]={0,0,1,-1};
+        for(int k = 0; k < 4; k++)
+        {
+            int ni = i + di[k];
+            int nj = j + dj[k];
+            if(ni >= 0 && ni < n && nj >=0 && nj < m && visited[ni][nj] == 0 && grid[ni][nj] == 1){
+                dfs(ni, nj, grid,visited,n,m);
             }
         }
     }
