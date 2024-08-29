066d3ccc9114fcf15138153f8b1d4c1b3c8f6ab2
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-08-30 03:37:44 +0530

    Time: 32 ms (33.45%) | Memory: 44.8 MB (62.75%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,81 +1,54 @@
-class Disjoinset{
-    int parent[];
+class Disjoint{
     int size[];
-    public Disjoinset(int n)
-    {
-        parent = new int[n + 1];
+    int parent[];
+    public Disjoint(int n){
         size = new int[n + 1];
-        for(int i =0 ; i <=n ; i++)
-        {
-            parent[i] = i;
+        parent = new int[n + 1];
+
+        for(int i = 0; i <= n; i++){
             size[i] = 1;
-        } 
-    }
-    public int getParent(int node)
-    {
-        if(parent[node] == node)
-        {
-            return node;
+            parent[i] = i;
         }
-        parent[node] = getParent(parent[node]);
-        return parent[node];
     }
-    public void unionBySize(int node1, int node2)
-    {
+    public int getParent(int node){
+        if(parent[node] == node) return node;
+
+        return parent[node] = getParent(parent[node]);
+    }
+    public void union(int node1,int node2){
         int up1 = getParent(node1);
         int up2 = getParent(node2);
 
-        int size1 = size[up1];
-        int size2 = size[up2];
-        if(up1 == up2) return;
-        if(size1 == size2)
-        {
-            parent[up1] = up2;
-            size[up2] *= 2;
-        }
-        else if(size[up1] < size[up2])
-        {
+        if(up1 == up2){
+            size[up1] =2 * size[up1];
+        }else if(size[up1] > size[up2]){
+            parent[up2] = up1;
+            size[up1] += size[up2];
+        }else{
             parent[up1] = up2;
             size[up2] += size[up1]; 
-        }
-        else{
-            parent[up2] = up1;
-            size[up1] += size[up2]; 
         }
     }
 }
 class Solution {
     public int removeStones(int[][] stones) {
+        Disjoint ds = new Disjoint(1001);
         int n = stones.length;
-        int maxRow = 0;
-        int maxCol = 0;
-        for(int i[]: stones)
-        {
-            maxRow = Math.max(maxRow , i[0]);
-            maxCol = Math.max(maxCol , i[1]);
-        }
-        Disjoinset ds = new Disjoinset(maxRow + maxCol + 1); 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i[]: stones)
-        {
-            int node1 = i[0]; // row
-            int node2 = maxRow + i[1] + 1; // col
-            int up1 = ds.getParent(node1);
-            int up2 = ds.getParent(node2);
-            if(up1 == up2) continue;
-            ds.unionBySize(node1, node2);
-            map.put(node1, 1);
-            map.put(node2, 1);
-        }
-        int components = 0;
-
-        for(Map.Entry<Integer,Integer> entry: map.entrySet())
-        {
-            if(ds.getParent(entry.getKey()) == entry.getKey())
-            {
-                components++;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                {
+                    ds.union(i, j);
+                }
             }
         }
-        return (n - components);
+        int parent[] = ds.parent;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(parent[i] == i){
+                count++;
+            }
+        }
+        return n - count;
     }
 }
