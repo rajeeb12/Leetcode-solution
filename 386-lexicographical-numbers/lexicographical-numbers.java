3370d0b6911b1f665caf019232b244d9f55c7008
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-09-21 19:52:10 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (28.23%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 1; i <= n; i++){
-            solve(i,set, ans, n);
+        for(int i = 1; i <= 9; i++)
+        {
+            solve(i, ans, n);
         }
         return ans;
     }
-    public void solve(int i,HashSet<Integer> set,List<Integer> ans,int n){
+    public void solve(int i,List<Integer> ans,int n)
+    {
         if(i > n) return;
 
-        if(!set.contains(i) && i <= n){
-            set.add(i);
-            ans.add(i);
-        }
+        ans.add(i);
 
-        for(int j = i * 10 ; j < (i * 10) + 10; j++)
+        for(int j = 0; j <= 9; j++)
         {
-            solve(j, set, ans, n);
+            int curnumber = i * 10 + j; // main 
+            if(curnumber <= n){
+                solve(curnumber, ans, n);
+            }else{
+                break;
+            }
         }
     }
 }
