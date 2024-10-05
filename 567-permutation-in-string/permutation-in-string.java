f9c88123a9bff29eb102ed6372caaf4c70d4bd98
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-10-05 12:11:22 +0530

    Time: 5 ms (96.14%) | Memory: 42.3 MB (94.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
-        int map1[]= new int[26];
-        int map2[]= new int[26];
-
-        for(int i = 0 ; i < s1.length(); i++)
-        {
-            map1[s1.charAt(i)-'a']++;
-            map2[s2.charAt(i)-'a']++;
-        }
-        if(Arrays.equals(map1, map2)) return true;
-        int left = 0;
-        int right = s1.length();
+        int map[] = new int[26];
+        for(char c: s1.toCharArray()) map[c-'a']++;
+        
+        int map2[] = new int[26];
+        int i = 0;
         int n = s2.length();
-        while(right < n)
+        for(int j = 0; j < n; j++)
         {
-            map2[s2.charAt(left) - 'a']--;
-            left++;
-
-            map2[s2.charAt(right) -'a']++;
-            right++;
-
-            if(Arrays.equals(map1, map2)) return true;
+            map2[s2.charAt(j) - 'a']++;
+            int curSize = j - i + 1;
+            if(curSize > s1.length())
+            {
+                map2[s2.charAt(i) - 'a']--;
+                i++;
+            }
+            
+            boolean flag = true;
+            for(int k = 0; k < 26; k++)
+            {
+                if(map[k] != map2[k])
+                {
+                    // System.out.print(i +" "+j+" "+map[k]+"| ");
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return true;
         }
         return false;
     }
