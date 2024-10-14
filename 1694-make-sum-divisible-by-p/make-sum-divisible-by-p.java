7fa544bbdbd4f48b73ed94afed4480767eb23e4c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-10-14 21:50:33 +0530

    Time: 24 ms (80.07%) | Memory: 58.3 MB (5.07%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,28 +1,25 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long n = nums.length;
+        int n = nums.length;
         long sum = 0;
         for(int i: nums) sum += i;
-
+        
         if(sum % p == 0) return 0;
-
-        long needed = sum % p;
-        HashMap<Long,Integer> map = new HashMap<>();
-        map.put(0L, -1);
-
-        long curSum = 0;
-        long minLength = n;
+        if(sum < p) return -1;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int eliminate = (int) (sum % p);
+        int csum = 0;
+        int ans = n;
         for(int i = 0; i < n; i++)
         {
-            curSum += nums[i];
-            long curMod =curSum % p;
-            long targetMod = (curMod - needed + p) % p;
-
-            if(map.containsKey(targetMod)){
-                minLength = Math.min(minLength, i - map.get(targetMod));
-            }
-            map.put(curMod, i);
+            csum = (csum + nums[i]) % p;
+            map.put(csum, i);
+            int want = (csum - eliminate + p) % p;
+            ans = Math.min(ans , i - map.getOrDefault(want , -n));
         }
-        return (minLength == n ? -1 : (int) minLength);
+
+        return (ans < n ? ans : -1);
     }
 }
