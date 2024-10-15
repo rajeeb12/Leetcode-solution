055cece8e539364100908be801f4c137e3c11938
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-10-15 18:54:06 +0530

    Time: 45 ms (77.51%) | Memory: 48.7 MB (84.29%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,27 +1,23 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int targetStart = times[targetFriend][0];
+        int ts = times[targetFriend][0];
         Arrays.sort(times, (a, b) -> a[0] - b[0]);
-        
-        PriorityQueue<Integer> available = new PriorityQueue<>();
-        for (int i = 0; i < times.length; ++ i) {
-            available.offer(i);
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        
-        for (int i = 0; i < times.length; ++ i) {
-            while (!pq.isEmpty() && pq.peek()[0] <= times[i][0]) {
-                available.offer(pq.poll()[1]);
+
+        PriorityQueue<Integer> availableSeats = new PriorityQueue<>();
+
+        for(int i = 0; i < times.length; i++) availableSeats.add(i);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+
+        for(int time[]: times)
+        {
+            while(!pq.isEmpty() && time[0] >= pq.peek()[0]){
+                availableSeats.add(pq.poll()[1]);
             }
-            
-            if (times[i][0] == targetStart) {
-                break;
-            }
-            
-            pq.offer(new int[]{times[i][1], available.poll()});
-        }
-        
-        return available.peek();
+            if(time[0] == ts) break;
+            int seat = availableSeats.poll();
+            pq.add(new int[]{time[1], seat});
+        } 
+        return availableSeats.poll();
     }
 }
