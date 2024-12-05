3b0463ae366a57a06be346698e905f5447d6ff7c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-12-06 02:59:09 +0530

    Time: 103 ms (5.03%) | Memory: 45.8 MB (22.61%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,25 +1,8 @@
 class Solution {
     public boolean canChange(String start, String target) {
         int n = start.length();
-        String ans1 = "";
-        String ans2 = "";
-        int l1 = 0 , r1 = 0, l2 = 0, r2 = 0; 
+        if(!start.replaceAll("_", "").equals(target.replaceAll("_",""))) return false;
 
-        for(int i = 0 ; i < n; i++){
-            char c = start.charAt(i);
-            char c2 = target.charAt(i);
-
-            if(c != '_'){
-                ans1 += c; 
-                l1 = (c == 'L' ? 1 : 0);
-                r1 = (c == 'R' ? 1 : 0);
-            }
-            if(c2 != '_'){
-                ans2 += c2;
-                l2 = (c2 == 'L' ? 1 : 0);
-                r2 = (c2 == 'R' ? 1 : 0);
-            }
-        }
         for(int i = 0 , j = 0 ; i < n && j < n; i++, j++){
             while(i < n && start.charAt(i) == '_'){
                 i++;
@@ -32,7 +15,7 @@ class Solution {
                 if(start.charAt(i) == 'R' && i > j) return false;
             }
         }
-        if(l1 != l2 || r1 != r2 || !ans1.equals(ans2)) return false;
+        
         
         return true;
     }
