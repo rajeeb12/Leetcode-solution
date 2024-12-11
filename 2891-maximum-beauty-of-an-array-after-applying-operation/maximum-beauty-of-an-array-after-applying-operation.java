2be5c01f6f360129d5c0b8b0fb812ba9a4c4224c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2024-12-11 22:35:19 +0530

    Time: 37 ms (82.97%) | Memory: 60.7 MB (22.77%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -3,27 +3,14 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
 
-        int max = 0;
-        for(int i = 0; i < n; i++){
-            int x = nums[i] + (2 * k);
-            int index = solve(nums, x);
-
-            max = Math.max(max, index - i + 1);
+        int i = 0, max = 0;
+        for(int j = 0; j < n; j++){
+            if(nums[j] > nums[i] + (2 * k)){
+                i++;
+            }
+            max = Math.max(max, j - i + 1);
         }
         return max;
     }
-    public int solve(int nums[],int x){
-        int i = 0, j = nums.length - 1;
-        int ans = 0;
-        while(i <= j){
-            int mid = (i + j) >> 1;
-            if(nums[mid] <= x){
-                ans = mid;
-                i = mid + 1;
-            }else{
-                j = mid - 1;
-            }
-        }
-        return ans;
-    }
+    
 }
