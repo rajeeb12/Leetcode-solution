20c1a50dfe31931b0f33062c1d298e89963cdb7a
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-01-07 23:50:08 +0530

    Time: 4 ms (86.21%) | Memory: 42.3 MB (54.32%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,22 +1,16 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        Arrays.sort(words, new Comparator<String>(){
-            public int compare(String a,String b){
-                return a.length() - b.length();
-            }
-        });
         List<String> ans = new ArrayList<>();
-        HashSet<String> set = new HashSet<>();
-        for(int k = words.length - 1; k >= 0; k--)
+
+        for(int i = 0; i < words.length; i++)
         {
-            System.out.print(words[k]+" ");
-            String s = words[k];
-            if(set.contains(s)) ans.add(s);
-            for(int i = 0; i < s.length(); i++){
-                for(int j = i; j < s.length(); j++){
-                    String substr = s.substring(i, j + 1);
-                    set.add(substr);
-                    //System.out.print(substr+" ");
+            for(int j = 0; j < words.length; j++){
+                if(i != j)
+                {
+                    if(words[j].contains(words[i])){
+                        ans.add(words[i]);
+                        break;
+                    }
                 }
             }
         }
