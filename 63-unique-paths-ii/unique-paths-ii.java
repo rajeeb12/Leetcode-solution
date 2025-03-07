6772bc419f0fc671c3433c0906d62e987633ac1b
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-07 22:50:11 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (99.85%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,27 +2,31 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
         int n = arr.length;
         int m = arr[0].length;
-        int dp[][] = new int[n + 1][m + 1];
+
+        //int dp[][] = new int[n + 1][m + 1];
+        int prev[] = new int[m];
 
         for(int i = 0; i < n ; i++)
         {
+            int cur[] = new int[m];
             for(int j = 0; j < m; j++)
             {
                 if(arr[i][j] == 1){
-                    dp[i][j] = 0;
+                    cur[j] = 0;
                 }
                 else if(i == 0 && j == 0){
-                    dp[i][j] = 1;
+                    cur[j] = 1;
                 }else{
                     int up = 0, left = 0;
 
-                    if(j > 0)left += dp[i][j - 1];
-                    if(i > 0)up += dp[i - 1][j];
-                    dp[i][j] = left + up;
+                    if(j > 0)left += cur[j - 1];
+                    if(i > 0)up += prev[j];
+                    cur[j] = left + up;
                 }
             }
+            prev = cur;
         }
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
         //return solve(n - 1, m - 1, og);
     }
     public int solve(int i,int j,int arr[][]){
