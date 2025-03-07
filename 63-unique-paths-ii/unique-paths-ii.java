92f7c3d45cb6ee30a99fa36cd25691fb11596386
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-07 22:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (37.85%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,23 +1,42 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
         int dp[][] = new int[n + 1][m + 1];
-        for(int i[]: dp) Arrays.fill(i, -1);
-        return solve(0,0,obstacleGrid,n,m,dp);
+
+        for(int i = 0; i < n ; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(arr[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    int up = 0, left = 0;
+
+                    if(j > 0)left += dp[i][j - 1];
+                    if(i > 0)up += dp[i - 1][j];
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+        //return solve(n - 1, m - 1, og);
     }
-    public int solve(int i,int j,int arr[][],int n,int m,int dp[][]){
-        if(i == n - 1 && j == m - 1 && arr[i][j] == 0) return 1;
+    public int solve(int i,int j,int arr[][]){
+        if(i >= 0 && j >= 0 && arr[i][j] == 1) return 0;
 
-        if(i == n || j == m || arr[i][j] == 1) return 0;
+        if(i < 0 || j < 0) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if(i == 0 && j == 0) return 1;
+        
+        int up = 0, left = 0;
 
-        int down = 0, right = 0;
+        left += solve(i, j - 1, arr);
+        up += solve(i - 1, j ,arr);
 
-        down += solve(i, j + 1, arr, n, m, dp);
-        right += solve(i + 1, j, arr, n, m,dp);
-
-        return dp[i][j] =  down + right;
+        return up + left;
     }
 }
