4809f8f87d6f407cbafc8f0bb8c5cd94c25f8ba2
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-09 02:15:21 +0530

    Time: 4 ms (50.75%) | Memory: 52.3 MB (20.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,43 +2,29 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int visited[][] = new int[n][m];
         int count = 0;
-        int visited[][]= new int[n][m];
-
-        for(int i = 0 ; i < n; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(grid[i][j] == '1' && visited[i][j] == 0)
-                {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1' && visited[i][j] == 0){
                     count++;
-                    dfs(i,j,grid, visited,n,m);
+                    dfs(i,j,grid,visited);
                 }
             }
         }
         return count;
     }
-    public void dfs(int i,int j,char[][] grid, int[][] visited,int n,int m)
-    {
-        visited[i][j] =1;
-
+    public void dfs(int i,int j,char grid[][],int visited[][]){
+        visited[i][j] = 1;
         int di[] = {1,-1,0,0};
         int dj[] = {0,0,1,-1};
 
-        for(int k = 0 ; k < 4 ; k++)
-        {
-            int next_row = i + di[k];
-            int next_col = j + dj[k];
-
-            if(validCo(next_row, next_col,n, m) && grid[next_row][next_col]=='1' && visited[next_row][next_col] == 0)
-            {
-                dfs(next_row, next_col,grid, visited, n, m);
+        for(int k = 0; k < 4; k++){
+            int ni = i + di[k];
+            int nj = j + dj[k];
+            if(ni >= 0 && nj >= 0 && ni < grid.length && nj < grid[0].length && visited[ni][nj] == 0 && grid[ni][nj] == '1'){
+                dfs(ni, nj, grid, visited);
             }
         }
-    }
-    public boolean validCo(int i,int j,int n,int m)
-    {
-        if(i >= 0 && j >= 0 && i < n && j < m) return true;
-        return false;
     }
 }
