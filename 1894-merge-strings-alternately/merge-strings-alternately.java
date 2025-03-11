43a9141a784156aa147e0e1877c98140482b0c44
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-12 03:02:27 +0530

    Time: 6 ms (22.82%) | Memory: 43.1 MB (9.03%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,29 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int p1 = 0, p2 = 0;
-        boolean turnP1 = true;
-        String res = "";
-        int n1 = word1.length();
-        int n2 = word2.length();
+        String result = "";
+        int min = Math.min( word1.length(), word2.length());
+        int index = 0;
+        for(int i = 0; i < min; i++)
+        {
+            result += word1.charAt(i);
+            result += word2.charAt(i);
+            index++;
+        }
+        while(index < word1.length()) result += word1.charAt(index++);
+        while(index < word2.length()) result += word2.charAt(index++);
 
-        while(p1 < n1 && p2 < n2){
-            if(turnP1){
-                res += word1.charAt(p1);
-                p1++;
-            }else{
-                res += word2.charAt(p2);
-                p2++;
-            }
-            turnP1 = !turnP1;
-        }
-        while(p1 < n1){
-            res += word1.charAt(p1);
-            p1++;
-        }
-        while(p2 < n2){
-            res += word2.charAt(p2);
-            p2++;
-        }
-        return res;
+        return result;
     }
 }
