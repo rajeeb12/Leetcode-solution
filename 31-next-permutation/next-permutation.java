c8c5c39c44824ba4eee2cb8e4f7274f86c500dff
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-12 19:41:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (36.83%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,38 +1,36 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int lindex = -1;
+        int index = -1;
         int n = nums.length;
-        for(int i = n - 2; i >= 0; i--)
-        {
+        for(int i =  n - 2; i >= 0; i--){
             if(nums[i] < nums[i + 1]){
-                lindex = i;
+                index = i;
                 break;
             }
         }
-        if(lindex == - 1)
+        if(index == -1) // descending order already
         {
             reverse(0, n - 1,nums);
             return;
         }
-        
-        for(int i = n - 1; i >= 0; i--)
-        {
-            if(nums[i] > nums[lindex]){
-                int temp = nums[lindex];
-                nums[lindex] = nums[i];
+
+        for(int i = n - 1; i >= 0; i--){
+            if(nums[i] > nums[index]){
+                int temp = nums[index];
+                nums[index] = nums[i];
                 nums[i] = temp;
-                break; 
+                break;
             }
         }
-        reverse(lindex + 1, n - 1, nums);
+        reverse(index + 1, n - 1, nums);
     }
-    public void reverse(int i,int j,int[] arr){
-        while(i <= j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+    public void reverse(int start,int end,int nums[]){
+        while(start <= end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
