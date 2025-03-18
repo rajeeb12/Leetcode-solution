23507238591971a30e25cd6364bdefbeb519c8dd
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-19 03:17:32 +0530

    Time: 3 ms (98.67%) | Memory: 55.7 MB (68.17%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,29 +1,16 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
         int n = nums.length;
-        int max = 1;
+        int maxLength = 0;
         for(int i = 0; i < n; i++){
-            if(i + 1 < n && (nums[i] & nums[i + 1]) == 0){
-                int j = i + 2;
-                int temp = nums[i] | nums[i + 1];
-                boolean flag = true;
-                while(flag && j < n){
-                    for(int k = 0; k < 31; k++){
-                        int bit1 = (temp >> k) & 1;
-                        int bit2 =  (nums[j] >> k) & 1;
-                        if(bit1 == 1 && bit2 == 1){
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(flag && j < n){
-                        temp |= nums[j];
-                        j++;
-                    }
-                }
-                max = Math.max(max, j - i);
+            int mask = 0;
+            for(int j = i; j < n; j++){
+                if((nums[j] & mask) != 0) break;
+
+                mask |= nums[j];
+                maxLength = Math.max(maxLength, j - i + 1);
             }
         }
-        return max;
+        return maxLength;
     }
 }
