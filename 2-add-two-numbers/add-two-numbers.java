351d2c51680ccf1abe047493f306f617cc94e229
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-20 22:57:47 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (13.91%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,41 +10,47 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
+        ListNode list1 = l1;
+        ListNode list2 = l2;
+
         ListNode dummy = new ListNode(0);
+
         ListNode cur = dummy;
-        while(l1 != null && l2 != null)
-        {
-            int sum = l1.val + l2.val + carry;
-            ListNode d = new ListNode(sum % 10);
-            cur.next = d;
-            cur = cur.next;
-            carry = (sum / 10); 
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while(l1 != null)
-        {
-            int sum = carry + l1.val;
-            ListNode d = new ListNode(sum % 10);
-            cur.next = d;
-            cur = cur.next;
+        
+        int carry = 0;
+
+        while(list1 != null && list2 != null){
+            int sum = list1.val + list2.val + carry;
+            ListNode node = new ListNode(0);
+            node.val = sum % 10;
             carry = sum / 10;
-            l1 = l1.next;
+            cur.next = node;
+            cur = node;
+            list1 = list1.next;
+            list2 = list2.next;
         }
-        while(l2 != null)
-        {
-            int sum = carry + l2.val;
-            ListNode d = new ListNode(sum % 10);
-            cur.next = d;
-            cur = cur.next;
+        while(list1 != null){
+            int sum = list1.val + carry;
+            int rem = sum % 10;
             carry = sum / 10;
-            l2 = l2.next;
+            ListNode node = new ListNode(rem);
+            cur.next = node;
+            cur = node;
+            list1 = list1.next;
         }
-        if(carry == 1)
-        {
-            ListNode d = new ListNode(carry % 10);
-            cur.next= d;
+        while(list2 != null){
+            int sum = list2.val + carry;
+            int rem = sum % 10;
+            carry = sum / 10;
+            ListNode node = new ListNode(rem);
+            cur.next = node;
+            cur = node;
+            list2 = list2.next;
+        }
+        if(carry != 0){
+            ListNode node = new ListNode(1);
+            cur.next = node;
+            cur = node;
         }
         return dummy.next;
     }
