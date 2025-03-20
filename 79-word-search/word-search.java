86abfcf55d7c0c48f1e5d9602cdd4722677a6468
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-20 23:16:34 +0530

    Time: 170 ms (36.45%) | Memory: 45.1 MB (17.88%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,57 +1,40 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        char arr[] = word.toCharArray();
-
-        int visited[][] = new int[n][m];
-        for(int i = 0 ; i < n; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(board[i][j] == arr[0])
-                {
-                    if(solve(i,j,board,visited,arr, n , m,1))
-                    {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0)){
+                    int visited[][] = new int[m][n];
+                    if(check(i,j,1,visited, board, word)){
                         return true;
                     }
                 }
             }
-        } 
+        }
         return false;
     }
-    public boolean solve(int row, int col, char[][] board,int[][] visited,char[] arr, int n,int m,int index)
-    {
-        if(index == arr.length)
-        {
-            return true;
-        }
+    public boolean check(int row,int col,int index,int visited[][],char[][] board,String word){
+        
+        if(index >= word.length()) return true;
+        
         visited[row][col] = 1;
 
         int di[] = {1,-1,0,0};
         int dj[] = {0,0,1,-1};
 
-        for(int i = 0 ; i < 4; i++)
-        {
-            int next_row = row + di[i];
-            int next_col = col + dj[i];
-            if(helper(next_row,next_col, n ,m))
-            {
-                if(board[next_row][next_col] == arr[index] && visited[next_row][next_col] == 0)
-                {
-                    if(solve(next_row,next_col,board,visited,arr, n , m, index + 1))
-                    {
-                        return true;
-                    }
-                }
+        for(int k = 0; k < 4; k++){
+            int nextRow = row + di[k];
+            int nextCol = col + dj[k];
+
+            if(nextRow >= 0 && nextCol >= 0 && nextRow < board.length && nextCol < board[0].length && board[nextRow][nextCol] == word.charAt(index) && visited[nextRow][nextCol] == 0 && check(nextRow, nextCol, index + 1, visited, board, word)){
+                return true;
             }
-        } 
+        }
+        
         visited[row][col] = 0;
+        
         return false;
-    }
-    public boolean helper(int next_row,int next_col,int n,int m)
-    {
-        if(next_row >=0 && next_col >= 0 && next_row < n && next_col < m) return true;
-        return false;
+
     }
 }
