4cb0ceec4503436152064474282cbbd11d136bb2
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-20 21:25:12 +0530

    Time: 5 ms (79.08%) | Memory: 46 MB (50.65%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,42 +1,47 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-        int inorder[] = new int[numCourses];
+        int inOrder[] = new int[numCourses];
+
         for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>());
         }
-        for(int i[]: prerequisites){
-            adj.get(i[1]).add(i[0]);
-            inorder[i[0]]++;
+
+        for(int p[]: prerequisites){
+            inOrder[p[0]]++;
+            adj.get(p[1]).add(p[0]);
         }
         Queue<Integer> q = new LinkedList<>();
+
         for(int i = 0; i < numCourses; i++){
-            if(inorder[i] == 0) q.add(i);
+            if(inOrder[i] == 0) q.add(i);
         }
-        List<Integer> temp = new ArrayList<>();
-        
-        int index = 0;
+        List<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()){
             int size = q.size();
+
             for(int i = 0; i < size; i++){
-                int node = q.poll();
-                temp.add(node);
-                
-                for(int adjNode: adj.get(node)){
-                    inorder[adjNode]--;
-                    if(inorder[adjNode] == 0){
-                        q.add(adjNode);
+                int u = q.poll();
+                ans.add(u);
+
+                for(int v: adj.get(u)){
+                    inOrder[v]--;
+
+                    if(inOrder[v] == 0){
+                        q.add(v);
                     }
                 }
             }
         }
         
-        if(temp.size() != numCourses) return new int[]{};
+        if(ans.size() != numCourses) return new int[]{};
 
-        int res[] = new int[numCourses];
-        for(int i = 0; i < numCourses; i++){
-            res[i] = temp.get(i);
+        int result[] = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            result[i] = ans.get(i);
         }
-        return res;
+
+        return result;
+
     }
 }
