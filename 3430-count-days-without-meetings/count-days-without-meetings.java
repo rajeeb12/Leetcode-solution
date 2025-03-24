3a29c296bf24cd6c43b709943fa1c2936bb5c7cf
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-03-24 13:35:59 +0530

    Time: 53 ms (22.56%) | Memory: 100.3 MB (74.41%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.java
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -1,27 +1,37 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        Arrays.sort(meetings, (a,b) -> a[0] - b[0]);
-
-        List<int[]> ls= new ArrayList<>();
-        int sday = meetings[0][0];
-        int eday = meetings[0][1];
-        int count = sday - 1; 
-        for(int i= 1; i < meetings.length; i++){
-            if(eday >= meetings[i][0]){
-                sday = Math.min(sday, meetings[i][0]);
-                eday = Math.max(eday, meetings[i][1]);
+        Arrays.sort(meetings, new Comparator<int[]>(){
+            public int compare(int a[],int b[]){
+                if(a[0] == b[0]){
+                    return Integer.compare(a[1] ,b[1]);
+                }
+                return Integer.compare(a[0], b[0]);
             }
-            else{
-                count += meetings[i][0] - 1 - eday;
-                int[] temp = new int[2];
-                temp[0] = sday;
-                temp[1] = eday;
-                ls.add(temp);
-                sday = meetings[i][0];
-                eday = meetings[i][1];
+        });
+
+        int start = meetings[0][0];
+        int end = meetings[0][1];
+        List<int[]> temp = new ArrayList<>();
+        for(int i = 1; i < meetings.length; i++){
+            if(meetings[i][0] <= end){
+                end = Math.max(meetings[i][1], end);
+                start = Math.min(start, meetings[i][0]);
+            }else{
+                temp.add(new int[]{start, end});
+                start = meetings[i][0];
+                end = meetings[i][1];
             }
         }
-        count += days - eday;
+        temp.add(new int[]{start, end});
+        int count = temp.get(0)[0] - 1;
+
+        for(int i = 1; i < temp.size(); i++){
+            int prev[] = temp.get(i - 1);
+            int cur[] = temp.get(i);
+            count += cur[0] - prev[1] - 1;
+        }
+        count += days - temp.get(temp.size() - 1)[1];
         return count;
+
     }
 }
