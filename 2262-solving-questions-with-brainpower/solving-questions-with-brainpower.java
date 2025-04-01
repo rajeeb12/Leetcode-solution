ab766abf450eacb635f8e71bd5d7ee957d481954
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-02 01:10:06 +0530

    Time: 7 ms (52.97%) | Memory: 78.6 MB (99.46%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,14 +1,21 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long dp[] = new long[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(0, questions, dp);
+        long dp[] = new long[200001];
+
+        for(int index = n - 1; index >= 0; index--)
+        {
+            long pick = 0, skip = 0;
+            pick = questions[index][0] + dp[index + questions[index][1] + 1];
+            skip = 0 + dp[index + 1];
+
+            dp[index] = Math.max(pick , skip);
+        }
+        return dp[0];
+        //return solve(0, questions, dp);
     }
     public long solve(int index,int questions[][],long dp[]){
         if(index >= questions.length) return 0;
-
-        if(dp[index] != -1) return dp[index];
 
         if(dp[index] != -1) return dp[index];
 
