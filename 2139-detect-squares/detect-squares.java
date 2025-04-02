cf5d5a41960f807f55747fadb38ed741981805bf
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-03 00:52:21 +0530

    Time: 157 ms (30.34%) | Memory: 49.2 MB (44.55%) - LeetSync

--- 2139-detect-squares/detect-squares.java
+++ 2139-detect-squares/detect-squares.java
@@ -1,24 +1,31 @@
 class DetectSquares {
-    int a[][];
     List<int[]> list;
+    HashMap<String,Integer> map;
     public DetectSquares() {
-        a = new int[1001][1001];
         list = new ArrayList<>();
+        map = new HashMap<>();
     }
     
     public void add(int[] point) {
-        a[point[0]][point[1]] += 1;
-        list.add(new int[]{point[0], point[1]});
+        String s = point[0]+"|"+point[1];
+        list.add(point);
+        map.put(s, map.getOrDefault(s, 0) + 1);
     }
     
     public int count(int[] point) {
-        int x1 = point[0] , y1 = point[1];
         int ans = 0;
-        for(int p3[]: list){
-            if(Math.abs(x1 - p3[0]) == 0 || Math.abs(x1 - p3[0]) != Math.abs(y1 - p3[1])) continue;
-
-            ans += a[x1][p3[1]] * a[p3[0]][y1];
-        }
+        int px = point[0];
+        int py = point[1];
+        for(int i = 0; i < list.size(); i++){
+            int x = list.get(i)[0];
+            int y = list.get(i)[1];
+            if(px == x || py == y || Math.abs(px - x) != Math.abs(py - y)) continue;
+            
+            String p1 = x+"|"+py; 
+            String p2 = px+"|"+y;
+            
+            ans += map.getOrDefault(p1, 0) * map.getOrDefault(p2, 0);
+        } 
         return ans;
     }
 }
