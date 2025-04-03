08991f722ea08f537cffff4e2008fe7498ac513b
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-04 00:23:58 +0530

    Time: 4 ms (11.94%) | Memory: 45.3 MB (15.21%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+        
+        List<int[]> temp = new ArrayList<>();
+        List<int[]> temp2 = new ArrayList<>();
+        temp2.add(newInterval);
 
-        ArrayList<int[]> temp = new ArrayList<>();
+        for(int i[]: intervals) temp2.add(i);
+        
+        Collections.sort(temp2, (a, b) -> a[0] - b[0]);
+        int start = temp2.get(0)[0];
+        int end = temp2.get(0)[1];
 
-        int start = newInterval[0];
-        int end = newInterval[1];
-
-        for(int[] i: intervals)
-        {
-            if(i[1] < newInterval[0])
-            {
-                temp.add(i);
-            }else if(i[0] > newInterval[1])
-            {
-                temp.add(newInterval);
-                newInterval = i;
+        for(int i = 1; i < temp2.size(); i++){
+            if(temp2.get(i)[0] <= end){
+                start = Math.min(start, temp2.get(i)[0]);
+                end = Math.max(end, temp2.get(i)[1]);
             }else{
-                newInterval[0] = Math.min(newInterval[0], i[0]);
-                newInterval[1] = Math.max(newInterval[1] , i[1]);
+                temp.add(new int[]{start, end});
+                start = temp2.get(i)[0];
+                end = temp2.get(i)[1];
             }
         }
-        temp.add(new int[]{newInterval[0], newInterval[1]});
+        temp.add(new int[]{start, end});
         int ans[][] = new int[temp.size()][2];
-        int index = 0;
-        for(int i[]: temp)
-        {
+        int index = 0 ;
+        for(int i[]: temp){
             ans[index][0] = i[0];
             ans[index][1] = i[1];
             index++;
