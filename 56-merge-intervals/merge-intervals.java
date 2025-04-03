e07fa0dc5bed0ac03b02900baeee5b71eda46705
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-04 00:33:56 +0530

    Time: 8 ms (86.86%) | Memory: 46.6 MB (71.19%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a,int[] b){
-                if(a[0] == b[0]){
-                    return a[1] - b[1];
-                }
-                return a[0] - b[0];
-            }
-        });
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> temp = new ArrayList<>();
 
         int start = intervals[0][0];
         int end = intervals[0][1];
-        List<int[]> list = new ArrayList<>();
 
-        for(int i = 1; i < intervals.length; i++){
+        for(int i = 1; i < n; i++){
             if(intervals[i][0] <= end){
-                start = Math.min(intervals[i][0], start);
-                end = Math.max(intervals[i][1], end);
+                start = Math.min(start, intervals[i][0]);
+                end = Math.max(end, intervals[i][1]);
             }else{
-                list.add(new int[]{start, end});
+                temp.add(new int[]{start, end});
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        list.add(new int[]{start, end});
-        int ans[][] = new int[list.size()][2];
-
-        for(int i = 0; i < list.size(); i++){
-            ans[i][0] = list.get(i)[0];
-            ans[i][1] = list.get(i)[1];
+        temp.add(new int[]{start, end});
+        int ans[][] = new int[temp.size()][2];
+        int index = 0;
+        for(int i[]: temp){
+            ans[index][0] = i[0];
+            ans[index][1] = i[1];
+            index++;
         }
         return ans;
     }
