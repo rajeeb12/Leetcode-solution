9025aa32acf9338801569fe44d0ce1c708d5cd5f
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-04 00:29:14 +0530

    Time: 1 ms (99.06%) | Memory: 45.1 MB (25.76%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,34 +1,33 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
-        
         List<int[]> temp = new ArrayList<>();
-        List<int[]> temp2 = new ArrayList<>();
-        temp2.add(newInterval);
+        int index = 0;
+        while(index < n && intervals[index][1] < newInterval[0]){
+            temp.add(new int[]{intervals[index][0],intervals[index][1]});
+            index++;
+        }
 
-        for(int i[]: intervals) temp2.add(i);
-        
-        Collections.sort(temp2, (a, b) -> a[0] - b[0]);
-        int start = temp2.get(0)[0];
-        int end = temp2.get(0)[1];
+        int start = newInterval[0];
+        int end = newInterval[1];
 
-        for(int i = 1; i < temp2.size(); i++){
-            if(temp2.get(i)[0] <= end){
-                start = Math.min(start, temp2.get(i)[0]);
-                end = Math.max(end, temp2.get(i)[1]);
+        for(int i = index; i < n; i++){
+            if(intervals[i][0] <= end){
+                start = Math.min(start, intervals[i][0]);
+                end = Math.max(end, intervals[i][1]);
             }else{
                 temp.add(new int[]{start, end});
-                start = temp2.get(i)[0];
-                end = temp2.get(i)[1];
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
         temp.add(new int[]{start, end});
         int ans[][] = new int[temp.size()][2];
-        int index = 0 ;
+        int index2 = 0;
         for(int i[]: temp){
-            ans[index][0] = i[0];
-            ans[index][1] = i[1];
-            index++;
+            ans[index2][0] = i[0];
+            ans[index2][1] = i[1];
+            index2++;
         }
         return ans;
     }
