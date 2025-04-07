25476d2d4f7cb984cf5561322537cd29e7b4c989
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-07 14:19:43 +0530

    Time: 82 ms (28.38%) | Memory: 57.4 MB (25.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,57 +1,27 @@
 class Solution {
-    boolean dp[][];
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-       
-        int totalsum = 0;
-        for(int i : nums)
-        {
-            totalsum += i;
-        }
-        if(totalsum % 2 != 0) return false;
-        totalsum /= 2;
-        dp = new boolean[n + 1][totalsum + 1];
+        int sum = 0;
+        
+        for(int i: nums) sum += i;
+        
+        if(sum % 2 == 1) return false;
 
-        for(int i = 0 ; i < n; i++)
-        {
-            dp[i][0] = true;
-        }
+        int target = sum / 2;
+        Boolean dp[][] = new Boolean[nums.length + 1][target + 1];
 
-        for(int index = n - 2; index >= 0; index--)
-        {
-            for(int target = 1 ; target <= totalsum ; target++)
-            {
-                boolean notpick = dp[index + 1][target];
-                boolean pick = false;
-                if(nums[index] <= target)
-                {
-                    pick = dp[index + 1][target - nums[index]];
-                }
-                dp[index][target] = (pick || notpick); 
-            }
-        }
-        return dp[0][totalsum];
-        //return solve(0, nums, totalsum);
+        return solve(nums.length - 1, target, nums, dp);
     }
-    // public boolean solve(int index,int[] nums,int target)
-    // {
-    //     if(index == nums.length)
-    //     {
-    //         if(target == 0)
-    //         {
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-    //     if(dp[index][target] != null) return dp[index][target];
+    public boolean solve(int index,int target,int nums[],Boolean dp[][]){
+        if(target < 0) return false;
+        if(index < 0){
+            if(target == 0) return true;
+            return false;
+        }
+        if(dp[index][target] != null) return dp[index][target];
 
-    //     boolean notpick = solve(index + 1, nums, target);
-    //     boolean pick = false;
-    //     if(nums[index] <= target)
-    //     {
-    //         pick = solve(index + 1, nums, target - nums[index]);
-    //     }
-    //     return dp[index][target] = (pick || notpick); 
+        boolean pick = solve(index - 1, target - nums[index], nums, dp);
+        boolean skip = solve(index - 1, target, nums, dp);
 
-    // }
+        return dp[index][target] = pick || skip;
+    }
 }
