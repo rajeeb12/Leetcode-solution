b38f353b3709c841703997ccaa69cfafd8f601d8
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-04-24 01:55:38 +0530

    Time: 1000 ms (5.26%) | Memory: 81.6 MB (5.85%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countLargestGroup(int n) {
+        int temp[] = new int[10000001];
+        int max = 0;
+
+        for(int i = 1; i <= n; i++){
+            int sum = 0;
+            int num = i;
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            temp[sum]++;
+            max = Math.max(max, temp[sum]);
+        }
+        int count = 0;
+        for(int i = 0; i < 10000001; i++){
+            if(max == temp[i]) count++;
+        }
+        return count;
+    }
+}
