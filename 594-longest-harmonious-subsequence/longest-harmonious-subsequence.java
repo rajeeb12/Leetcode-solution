17b19da84358a560700b5f80ea5be87612615a27
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-07-01 01:20:39 +0530

    Time: 18 ms (26.96%) | Memory: 45.4 MB (78.85%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -1,32 +1,20 @@
-class Pair{
-    int index;
-    int count;
-    public Pair(int _index,int _count){
-        this.index = _index;
-        this.count= _count;
-    }
-}
 class Solution {
     public int findLHS(int[] nums) {
         int n = nums.length;
-        HashMap<Integer,Pair> right = new HashMap<>();
+        HashMap<Integer,Integer> right = new HashMap<>();
         
         for(int i = 0; i < n; i++){
             if(right.containsKey(nums[i])){
-                Pair temp = right.get(nums[i]);
-                int count = temp.count;
-                int index = temp.index;
-
-                right.put(nums[i], new Pair(Math.min(index, i) , count + 1));
+                right.put(nums[i], right.get(nums[i]) + 1);
             }
             else{
-                right.put(nums[i] , new Pair(i, 1));
+                right.put(nums[i] , 1);
             }
         }
         int ans = 0;
         for(int i = 0; i < n; i++){
             if(right.containsKey(nums[i] - 1)){
-                ans = Math.max(ans, right.get(nums[i]).count + right.get(nums[i] -  1).count);
+                ans = Math.max(ans, right.get(nums[i]) + right.get(nums[i] -  1));
             }
         }
         return ans;
