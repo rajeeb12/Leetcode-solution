6d7d23d820d6a1851af4b3f75925b60c620ffc7b
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-09-01 09:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (8.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,23 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int lmax[] = new int[n];
-        int rmax[] = new int[n];
-        lmax[0] = height[0];
-        rmax[n - 1] = height[n - 1];
+        int lmax = 0, rmax = 0, i = 0, j = n - 1, total = 0;
 
-        for(int i = 1; i < n; i++){
-            lmax[i] = Math.max(height[i], lmax[i - 1]);
-        }
-        for(int i = n - 2; i >= 0; i--){
-            rmax[i] = Math.max(height[i], rmax[i + 1]);
-        }
-        int total = 0;
-        for(int i = 0; i < n; i++){
-            if(lmax[i] > height[i] && rmax[i] > height[i]){
-                total += Math.min(lmax[i] , rmax[i]) - height[i];
+        while(i < j){
+            if(height[i] <= height[j]){
+                if(lmax > height[i]){
+                    total += lmax - height[i];
+                }else{
+                    lmax = height[i];
+                }
+                i++;
+            }else{
+                if(rmax > height[j]){
+                    total += rmax - height[j];
+                }else{
+                    rmax = height[j];
+                }
+                j--;
             }
         }
         return total;
