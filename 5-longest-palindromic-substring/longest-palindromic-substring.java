76ca01a5d7b0e91e3f716b58f7f390de345a42e8
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-09-13 23:31:24 +0530

    Time: 118 ms (32.82%) | Memory: 50.2 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,30 +1,26 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int dp[][] = new int[1001][1001];
-
-        for(int i[]: dp) Arrays.fill(i, -1);
+        boolean dp[][] = new boolean[1001][1001];
 
         int n = s.length(), start = -1, end = -1, max = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(solve(i, j, s, dp) == 1 && j - i + 1 > max){
-                    start = i;
-                    end = j;
-                    max = j - i + 1;
+
+        for(int len = 1; len <= n; len++){
+            for(int i = 0; i + len - 1 < n; i++){
+                int j = i + len - 1;
+
+                if(s.charAt(i) == s.charAt(j)){
+                    if(len <= 2 || dp[i + 1][j - 1]){
+                        dp[i][j] = true;
+                        if(j - i + 1 > max){
+                            start = i;
+                            end = j;
+                            max = j - i + 1;
+                        }
+                    }
                 }
             }
         }
         if(start != -1) return s.substring(start, end + 1);
         return "";
-    }
-    public int solve(int i,int j,String s,int dp[][]){
-        if(i > j) return 1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = solve(i + 1, j - 1, s, dp);
-        }
-        return dp[i][j] = 0;
     }
 }
