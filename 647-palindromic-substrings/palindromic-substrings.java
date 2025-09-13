c0d4da254b498ceb21e77f09a286be8c7a6b299f
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-09-14 00:19:39 +0530

    Time: 823 ms (10.44%) | Memory: 46 MB (10.44%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,32 +1,21 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        boolean dp[][] = new boolean[n + 1][n + 1];
-        int count = 0; 
-
-        for(int g = 0 ; g < n ; g++)
-        {
-            for(int i = 0, j = g; j < n ; i++, j++)
-            {
-                if(g == 0)
-                {
-                    dp[i][j] = true;
-                }
-                else if(g == 1)
-                {
-                    dp[i][j] = s.charAt(i) == s.charAt(j);
-                }else{
-                    if(s.charAt(i) == s.charAt(j))
-                    {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
-                }
-                if(dp[i][j])
-                {
-                    count++;
-                }
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                String substr = s.substring(i, j + 1);
+                if(isPalindrome(substr)) count++;
             }
         }
         return count;
+    }
+    public boolean isPalindrome(String s){
+        int l = 0, r = s.length() - 1;
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
     }
 }
