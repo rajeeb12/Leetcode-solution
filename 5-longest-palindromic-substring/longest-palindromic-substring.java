9d383bba07ad69fb26b68cd333ebe06ce4d494cc
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-09-13 23:16:32 +0530

    Time: 296 ms (20.64%) | Memory: 56.4 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,40 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int start = 0;
-        int end = 0;
-        int len = 0;
-        int n = s.length();
-        for(int i = 0; i < n ; i++)
-        {
-            int left = i, right = i;
-            while(left >= 0 && right < n && s.charAt(left) ==s.charAt(right))
-            {
-                int curmax = right - left + 1; 
-                if(len < curmax)
-                {
-                    len = right - left + 1;
-                    start = left;
-                    end = right;
+        int dp[][] = new int[1001][1001];
+
+        for(int i[]: dp) Arrays.fill(i, -1);
+
+        int n = s.length(), start = -1, end = -1, max = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(solve(i, j, s, dp) == 1 && j - i + 1 > max){
+                    start = i;
+                    end = j;
+                    max = j - i + 1;
                 }
-                left--;
-                right++;
             }
-            
-            left = i ; right = i + 1;
-            while(left >= 0 && right < n && s.charAt(left) ==s.charAt(right))
-            {
-                int curmax = right - left + 1; 
-                if(len < curmax)
-                {
-                    len = right - left + 1;
-                    start = left;
-                    end = right;
-                }
-                left--;
-                right++;
-            }
-            
         }
-        return s.substring(start,end + 1);
+        if(start != -1) return s.substring(start, end + 1);
+        return "";
+    }
+    public int solve(int i,int j,String s,int dp[][]){
+        if(i > j) return 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = solve(i + 1, j - 1, s, dp);
+        }
+        return dp[i][j] = 0;
     }
 }
