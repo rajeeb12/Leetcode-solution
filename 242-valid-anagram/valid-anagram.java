50b8a30b7afbb3d16ca3a0444c4094a5b103e670
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-10-15 20:00:51 +0530

    Time: 2 ms (97.62%) | Memory: 43.5 MB (60.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
         int count[] = new int[27];
         for(char ch: s.toCharArray()){
             count[ch-'a']++;
