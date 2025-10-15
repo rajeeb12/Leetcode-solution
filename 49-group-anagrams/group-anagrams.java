a63f2f747e1445b587470eb7e05b030c71b302ea
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-10-15 20:33:36 +0530

    Time: 8 ms (38.10%) | Memory: 47.5 MB (97.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,30 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
-        int count[] = new int[26];
+
         for(String str: strs){
-            Arrays.fill(count, 0);
-
-            for(char c: str.toCharArray()){
-                count[c-'a']++;
+            char arr[] = str.toCharArray();
+            Arrays.sort(arr);
+            String str_ = new String(arr);
+            if(!map.containsKey(str_)){
+                map.put(str_, new ArrayList<>());
             }
-            StringBuilder temp = new StringBuilder();
-            for(int i = 0; i < 26; i++){
-                temp.append("#");
-                temp.append(count[i]);
-            }
-            String key = temp.toString();
-
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
-            }
-            map.get(key).add(str);
+            map.get(str_).add(str);
         }
         List<List<String>> ans = new ArrayList<>();
-
-        for(Map.Entry<String,List<String>> e: map.entrySet()){
-            List<String> temp = e.getValue();
-            ans.add(temp);
+        for(Map.Entry<String, List<String>> e: map.entrySet()){
+            ans.add(e.getValue());
         }
         return ans;
     }
