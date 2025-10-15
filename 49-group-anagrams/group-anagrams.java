6837e6968365618cbeac7ad96b251c8d98577572
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2025-10-15 21:22:13 +0530

    Time: 20 ms (14.98%) | Memory: 48.1 MB (45.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,18 +3,31 @@ class Solution {
         HashMap<String,List<String>> map = new HashMap<>();
 
         for(String str: strs){
-            char arr[] = str.toCharArray();
-            Arrays.sort(arr);
-            String str_ = new String(arr);
-            if(!map.containsKey(str_)){
-                map.put(str_, new ArrayList<>());
+            String hash = getHash(str);
+            if(!map.containsKey(hash)){
+                map.put(hash, new ArrayList<>());
             }
-            map.get(str_).add(str);
+            map.get(hash).add(str);
         }
         List<List<String>> ans = new ArrayList<>();
         for(Map.Entry<String, List<String>> e: map.entrySet()){
             ans.add(e.getValue());
         }
         return ans;
+        // TC - O(n * (O(mlogm) + O(m)))
+        // SC - O(n)
+    }
+    public String getHash(String s){
+        int arr[] = new int[26];
+        for(char ch: s.toCharArray()){
+            arr[ch-'a']++;
+        }
+        String hash = "";
+        for(int i = 0; i < 26; i++){
+            if(arr[i] != 0){
+                hash += ((char)(i + 'a'))+""+ arr[i];
+            }
+        }
+        return hash;
     }
 }
