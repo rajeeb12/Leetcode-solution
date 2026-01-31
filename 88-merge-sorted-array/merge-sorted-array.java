59aa3d61255d25af77f17b28ac5b804f896d683c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-02-01 01:48:19 +0530

    Time: 12 ms (3.89%) | Memory: 44.2 MB (11.76%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,17 +1,43 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) return;
+        if(m == 0){
+            int index = 0;
+            for(int i = 0; i < nums2.length; i++){
+                nums1[index++] = nums2[i];
+            }
+            return;
+        }
         int dummy[] = new int[m + n];
-        int index = 0;
-        for(int i = 0; i < m; i++){
+        // two pointers
+        int size = Math.min(m, n);
+        int i = 0, j = 0, index = 0;
+
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                dummy[index] = nums1[i];
+                i++;
+            }else{
+                dummy[index] = nums2[j];
+                j++;
+            }
+            System.out.print(dummy[index]);
+            index++;
+        }
+        while(i < m){
             dummy[index++] = nums1[i];
+            i++;
         }
-        for(int i = 0; i < n; i++){
-            dummy[index++] = nums2[i];
+        while(j < n){
+            dummy[index++] = nums2[j];
+            j++;
         }
-        Arrays.sort(dummy); // o(nlogn)
-        for(int i = 0; i < nums1.length; i++){ // O(m + n);
-            nums1[i] = dummy[i];
+        for(int k = 0; k < nums1.length; k++){
+            nums1[k] = dummy[k];
         }
-        // O(nlogn) + O(n + m) 
+
+        // dummy - 1, 2, 2, 3, 5, 6
+        // i = 0 -> 1 -> 2 -> 3
+        // j = 0 -> 1 -> 2 -> 3
     }
 }
