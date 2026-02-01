ec3ccf4ab8c530bb8fe6d4722d5ef82561a253d0
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-02-01 06:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (71.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -8,36 +8,24 @@ class Solution {
             }
             return;
         }
-        int dummy[] = new int[m + n];
-        // two pointers
-        int size = Math.min(m, n);
-        int i = 0, j = 0, index = 0;
+        // swapping and two pointers
+        int i = m - 1, j = n - 1, index = m + n - 1;
 
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                dummy[index] = nums1[i];
-                i++;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                int temp = nums1[i];
+                nums1[index] = temp;
+                nums1[i] = 0;
+                i--;
             }else{
-                dummy[index] = nums2[j];
-                j++;
+                nums1[index] = nums2[j];
+                j--;
             }
-            System.out.print(dummy[index]);
-            index++;
+            index--;
         }
-        while(i < m){
-            dummy[index++] = nums1[i];
-            i++;
+        while(j >= 0){
+            nums1[index] = nums2[j--];
+            index--;
         }
-        while(j < n){
-            dummy[index++] = nums2[j];
-            j++;
-        }
-        for(int k = 0; k < nums1.length; k++){
-            nums1[k] = dummy[k];
-        }
-
-        // dummy - 1, 2, 2, 3, 5, 6
-        // i = 0 -> 1 -> 2 -> 3
-        // j = 0 -> 1 -> 2 -> 3
     }
 }
