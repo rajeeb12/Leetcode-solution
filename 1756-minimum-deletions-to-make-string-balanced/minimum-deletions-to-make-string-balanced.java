75206661f51ec2c3cce1ff5bef01ac65fdf5a862
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-02-08 15:45:28 +0530

    Time: 43 ms (34.91%) | Memory: 47.9 MB (45.84%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int map[][] = new int[2][n], countB = 0, countA = 0;
-
-        for(int i = 0; i < n; i++){
-            map[0][i] = countB;
-            map[1][n - i - 1] = countA;
-            countB += (s.charAt(i) == 'b' ? 1 : 0);
-            countA += (s.charAt(n - i - 1) == 'a' ? 1 : 0);
+        int countA = 0, countB = 0;
+        for(char c: s.toCharArray()){
+            if(c == 'a') countA++;
         }
-        
         int result = (int)1e9;
-        for(int i = 0; i < n; i++){
-            result =  Math.min(map[0][i] + map[1][i], result);
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a') countA--;
+            result = Math.min(result, countA + countB);
+            if(s.charAt(i) == 'b') countB++;
         }
         return result;
-
     }
 }
