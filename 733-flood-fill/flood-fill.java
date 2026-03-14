118de1c626bcb016036ffb59ccbc71890e9393ce
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-15 00:57:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (60.38%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int track[][] = new int[image.length][image[0].length];
-        for(int i[]: track){
-            Arrays.fill(i, -1);
-        }
-        dfs(image,sr, sc, color, image[sr][sc], track);
+        if(color == image[sr][sc]) return image;
+        
+        dfs(image,sr, sc, color, image[sr][sc]);
         return image;
     }
-    public void dfs(int image[][],int r,int c,int color,int originalColor,int track[][]){
-        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length || track[r][c] != -1) return;
+    public void dfs(int image[][],int r,int c,int color,int originalColor){
+        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length) return;
 
         if(image[r][c] != originalColor) return;
 
         image[r][c] = color;
-        track[r][c] = 1;
 
-        dfs(image, r + 1, c, color, originalColor, track);
-        dfs(image, r - 1, c, color, originalColor, track);
-        dfs(image, r, c + 1, color, originalColor, track);
-        dfs(image, r, c - 1, color, originalColor, track);
+        dfs(image, r + 1, c, color, originalColor);
+        dfs(image, r - 1, c, color, originalColor);
+        dfs(image, r, c + 1, color, originalColor);
+        dfs(image, r, c - 1, color, originalColor);
     }
 }
