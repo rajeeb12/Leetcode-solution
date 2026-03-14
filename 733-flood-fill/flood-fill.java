f5ce07a60f720de944a8fc3e8a96e11260c0a633
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-15 00:52:52 +0530

    Time: 1 ms (44.84%) | Memory: 46.9 MB (40.93%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-        int track[][] = new int[n][m];
+        int track[][] = new int[image.length][image[0].length];
         for(int i[]: track){
             Arrays.fill(i, -1);
         }
-        int di[] = {1,-1,0,0};
-        int dj[] = {0,0,1,-1};
-        Queue<int[]> q =new LinkedList<>();
-        q.add(new int[]{sr, sc});
-        track[sr][sc] = 1;
-        int startingColor = image[sr][sc];
-        image[sr][sc] = color;
-        while(!q.isEmpty()){
-            int cor[] = q.poll();
-            for(int itr = 0; itr < 4; itr++){
-                int nexti = di[itr] + cor[0];
-                int nextj = dj[itr] + cor[1];
-                if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < m && image[nexti][nextj] == startingColor && track[nexti][nextj] == -1){
-                    image[nexti][nextj] = color;
-                    track[nexti][nextj] = 1;
-                    q.add(new int[]{nexti, nextj});
-                }
-            }
-        }
+        dfs(image,sr, sc, color, image[sr][sc], track);
         return image;
+    }
+    public void dfs(int image[][],int r,int c,int color,int originalColor,int track[][]){
+        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length || track[r][c] != -1) return;
+
+        if(image[r][c] != originalColor) return;
+
+        image[r][c] = color;
+        track[r][c] = 1;
+
+        dfs(image, r + 1, c, color, originalColor, track);
+        dfs(image, r - 1, c, color, originalColor, track);
+        dfs(image, r, c + 1, color, originalColor, track);
+        dfs(image, r, c - 1, color, originalColor, track);
     }
 }
