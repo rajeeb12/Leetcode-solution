12464491e7631f5f037b5adffa0ea96884cd07e5
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-15 00:45:37 +0530

    Time: 1 ms (44.84%) | Memory: 47.2 MB (9.72%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,49 +1,27 @@
-class Pair{
-    int i;
-    int j;
-    public Pair(int _i,int _j)
-    {
-        this.i = _i;
-        this.j = _j;
-    }
-}
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        
-        int curColor = image[sr][sc];
         int n = image.length;
         int m = image[0].length;
-        int di[]={1,-1,0,0};
-        int dj[]={0,0,1,-1};
-        int visited[][] = new int[n][m];
-
-        while(!q.isEmpty())
-        {
-            int size= q.size();
-            for(int i = 0; i < size; i++)
-            {
-                Pair p = q.poll();
-                int x = p.i;
-                int y = p.j;
-
-                image[x][y] = color;
-
-                for(int k = 0; k < 4; k++)
-                {
-                    int ni = x + di[k];
-                    int nj = y + dj[k];
-                    if(ni >= 0 && nj >= 0 && ni < n && nj < m){
-                        if(image[ni][nj] == color) continue;
-
-                        if(image[ni][nj] == curColor)
-                        {
-                            visited[ni][nj] = 1;
-                            q.add(new Pair(ni,nj));
-                        }
-                    }
+        int track[][] = new int[n][m];
+        for(int i[]: track){
+            Arrays.fill(i, -1);
+        }
+        int di[] = {1,-1,0,0};
+        int dj[] = {0,0,1,-1};
+        Queue<int[]> q =new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        track[sr][sc] = 1;
+        int startingColor = image[sr][sc];
+        image[sr][sc] = color;
+        while(!q.isEmpty()){
+            int cor[] = q.poll();
+            for(int itr = 0; itr < 4; itr++){
+                int nexti = di[itr] + cor[0];
+                int nextj = dj[itr] + cor[1];
+                if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < m && image[nexti][nextj] == startingColor && track[nexti][nextj] == -1){
+                    image[nexti][nextj] = color;
+                    track[nexti][nextj] = 1;
+                    q.add(new int[]{nexti, nextj});
                 }
             }
         }
