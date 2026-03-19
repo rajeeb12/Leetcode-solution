994abc7d18c621a70eb7bcfb8fa3bb1f3e0b0237
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-19 19:49:43 +0530

    Time: 1 ms (99.74%) | Memory: 50.3 MB (67.79%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,53 +1,24 @@
 class Solution {
     int dp[][];
     public int minPathSum(int[][] grid) {
+
         int n = grid.length;
         int m = grid[0].length;
-        //dp = new int[n + 1][m + 1];
-        int prev[]= new int[m + 1];
 
-        for(int i = 0 ; i < n ; i++)
-        {
-            int cur[]= new int[m + 1];
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(i == 0 & j == 0) cur[j] = grid[i][j];
-                else{
-                    int up= grid[i][j];
-                    int left = grid[i][j]; 
-                    if(i > 0)
-                    {
-                        up += prev[j];
-                    }else{
-                        up += (int)1e9;
-                    }
+        dp = new int[n + 1][m + 1];
+        for(int i[]: dp) Arrays.fill(i, - 1);
 
-                    if(j > 0){
-                        left += cur[j - 1];
-                    }else{
-                        left += (int)1e9;
-                    }
-                    cur[j] = Math.min(up, left);
-                }
-            }
-            prev = cur;
-        }
-        //return solve(grid, n -1 , m -1);
-        return prev[m - 1];
+        return solve(n - 1, m - 1, grid);
     }
-    public int solve(int[][] grid,int i, int j)
-    {
-        if(i < 0 || j < 0) return (int)1e9;
-        if(i == 0 && j == 0)
-        {
-            return grid[0][0];
-        }
+    public int solve(int row,int col,int grid[][]){
+        if(row < 0 || col < 0) return (int)1e9;
+        if(row == 0 && col == 0) return grid[row][col];
 
-        if(dp[i][j] != - 1) return dp[i][j];
+        if(dp[row][col] != -1) return dp[row][col];
 
-        int up = grid[i][j] + solve(grid, i - 1, j);
-        int left = grid[i][j] + solve(grid, i , j - 1);
+        int up = grid[row][col] + solve(row - 1, col, grid);
+        int left = grid[row][col] + solve(row, col - 1, grid);
 
-        return dp[i][j] = Math.min(left, up);
+        return dp[row][col] = Math.min(up, left);
     }
 }
