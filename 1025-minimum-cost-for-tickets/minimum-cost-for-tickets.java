3975fde2ca93c98b237139a600a8b51d842213ad
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-23 14:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (68.78%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,51 +1,29 @@
 class Solution {
     int dp[];
     public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
         dp = new int[days.length + 1];
-        //Arrays.fill(dp, -1);
-
-        for(int skip = n - 1; skip >= 0; skip--)
-        {
-            int oneDay = costs[0] + dp[skip + 1];
-
-            int i;
-            for(i = skip; i < n && days[i] < days[skip] + 7; i++);
-
-            int sevenDay = costs[1] + dp[i];
-
-            for(i = skip; i < n && days[i] < days[skip] + 30; i++);
-
-            int thirtyDay = costs[2] + dp[i];
-
-            dp[skip] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
-        }
-        return dp[0];
-
-        //int ans = solve(costs, days,0);
-        //return (ans == (int)1e9 ? 0: ans);
+        Arrays.fill(dp, -1);
+        return solve(0, days, costs);
     }
-    public int solve(int[] costs,int[] days,int skip)
-    {
-        int n = days.length;
-        if(skip >= days.length){
-            return 0;
+    public int solve(int index,int days[],int costs[]){
+        if(index >= days.length) return 0;
+
+        if(dp[index] != -1) return dp[index];
+
+        int one = costs[0] + solve(index + 1, days, costs);
+        int maxDaysForSevenDaysPass = days[index] + 7;
+        int j = index;
+        while(j < days.length && days[j] < maxDaysForSevenDaysPass){
+            j++;
         }
+        int seven = costs[1] + solve(j, days, costs);
+        int maxDaysForThirtyDaysPass = days[index] + 30;
+        j = index;
+        while(j < days.length && days[j] < maxDaysForThirtyDaysPass){
+            j++;
+        }
+        int thirty = costs[2] + solve(j, days, costs);
 
-        if(dp[skip] != -1) return dp[skip];
-
-        int oneDay = costs[0] + solve(costs,days, skip + 1);
-        int sevenDay = (int)1e9, thirtyDay = (int)1e9;
-
-        int i;
-        for(i = skip; i < n && days[i] < days[skip] + 7; i++);
-
-        sevenDay = costs[1] + solve(costs, days, i);
-
-        for(i = skip; i < n && days[i] < days[skip] + 30; i++);
-
-        thirtyDay = costs[2] + solve(costs, days, i);
-
-        return dp[skip] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+        return dp[index] = Math.min(one, Math.min(seven, thirty));
     }
 }
