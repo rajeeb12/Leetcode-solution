b535e993171c2a66dbfcc57a02086489fa3b82a8
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-25 02:15:17 +0530

    Time: 21 ms (92.09%) | Memory: 44.4 MB (81.50%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,34 +1,15 @@
 class Solution {
     int dp[];
-    public int numSquares(int j) {
-        dp = new int[j + 1];
+    public int numSquares(int limit) {
+        dp = new int[limit + 1];
 
-        for(int n = 1 ; n <= j; n++)
-        {
-            int ans = (int)1e9;
-            for(int i = 1; i * i <= n; i++)
-            {
-                ans = Math.min(1 + dp[n - i *i], ans);
+        for(int n = 1; n <= limit; n++){
+            int take = (int)1e9;
+            for(int number = 1; number * number <= n; number++){
+                take = Math.min(take, 1 + dp[n - (number * number)]);
             }
-            dp[n] = ans;
+            dp[n] = take;
         }
-        return dp[j];
-        //return solve(i);
-    }
-    public int solve(int n)
-    {
-        if(n == 0)
-        {
-            return 0;
-        }
-        if(dp[n] != -1) return dp[n];
-        
-        int ans = (int)1e9;
-        for(int i = 1; i * i <= n; i++)
-        {
-            ans = Math.min(1 + solve(n - i *i), ans);
-        }
-        return dp[n] = ans;
-
+        return dp[limit];
     }
 }
