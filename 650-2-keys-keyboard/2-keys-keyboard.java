610d4cd409da6b0418eb237bd12cdbf8e3125139
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-03-24 22:56:28 +0530

    Time: 6 ms (49.61%) | Memory: 42.4 MB (35.62%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -2,8 +2,18 @@ class Solution {
     int dp[];
     public int minSteps(int n) {
         dp = new int[n  + 1];
-        Arrays.fill(dp, -1);
-        return solve(n);
+        
+        for(int count = 2; count <= n; count++){
+            int res = n;
+            for(int i = count / 2; i >= 1; i--){
+                if(count % i == 0){
+                    res = Math.min(res, 1 + dp[i] + (count / i) - 1);
+                    break;
+                }
+            }
+            dp[count] = res;
+        }
+        return dp[n];
     }
     public int solve(int n){
         if(n == 1) return 0;
