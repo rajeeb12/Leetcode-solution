b3c5740f37a861c3754cbf5d8ae8bfbece582d51
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-16 04:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (86.25%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,32 +1,13 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
-        if(words[startIndex].equals(target)) return 0;
-
-        HashMap<String, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i < words.length; i++){
-            String word = words[i];
-            if(!map.containsKey(word)){
-                map.put(word, new ArrayList<>());
-            }
-            map.get(word).add(i);
-        }
-        
-        if(!map.containsKey(target)) return -1;
-
-        List<Integer> ls = map.get(target);
         int n = words.length;
-        int minDistance = (int)1e9;
-        for(int index : ls){
-            int right = 0,left = 0;
-            if(index > startIndex){
-                right = index - startIndex;
-                left = (n - index - 1) + startIndex + 1;
-            }else{
-                right = (n - startIndex - 1) + index + 1;
-                left = startIndex - index;
+        int ans = n + 1;
+        for(int i = 0; i < n; i++){
+            if(words[i].equals(target)){
+                int x = Math.abs(i - startIndex);
+                ans = Math.min(ans, Math.min(x, n - x));
             }
-            minDistance = Math.min(minDistance, Math.min(left, right));
         }
-        return minDistance;
+        return (ans == n  + 1 ? -1 : ans);
     }
 }
