8b093ebbcade75b57bc5bd57fb250e5a42830f4c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-19 20:24:05 +0530

    Time: 2 ms (100.00%) | Memory: 90.4 MB (7.80%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -1,35 +1,11 @@
 class Solution {
     public int maxDistance(int[] nums1, int[] nums2) {
-        int max = 0;
-        int n = nums1.length;
-        for(int i = 0; i < nums2.length; i++){
-            int minIndex = -1;
-            if(i < nums1.length){
-                if(nums1[i] <= nums2[i]){
-                    minIndex = getIndex(0, i, nums1,nums2, i);
-                }
-            }else{
-                if(nums1[n - 1] <= nums2[i]){
-                    minIndex = getIndex(0, n - 1, nums1,nums2, i);
-                }
-            }
-            if(minIndex != -1){
-                max = Math.max(max, i - minIndex);
-            }
+        int i = 0, j = 0, max = 0;
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] > nums2[j]) i++;
+            j++;
         }
-        return max;
-    }
-    public int getIndex(int left,int right,int nums1[], int nums2[],int i){
-        int minIndex = -1;
-        while(left <= right){
-            int mid= (left + right) >> 1;
-            if(nums1[mid] <= nums2[i]){
-                right = mid - 1;
-                minIndex = mid;
-            }else{
-                left = mid + 1;
-            } 
-        }
-        return minIndex;
+        return Math.max(0, j - i - 1);
     }
 }
