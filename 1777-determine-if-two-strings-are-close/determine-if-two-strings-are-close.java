2bb7b9bfbc7d9045ac245244a5c6b2cc0f3aab54
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-22 02:25:43 +0530

    Time: 9 ms (99.87%) | Memory: 48.1 MB (25.25%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,31 +1,24 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         if(word1.length() != word2.length()) return false;
-        int map1[] = new int[27];
-        int map2[] = new int[27];
-
-        for(char ch : word1.toCharArray()){
-            map1[ch-'a']++;
+        int map1[] = new int[26];
+        int map2[] = new int[26];
+        for(char c: word1.toCharArray()){
+            map1[c-'a']++;
         }
-        for(char ch : word2.toCharArray()){
-            map2[ch-'a']++;
-        }
-
-        for(int i = 0 ; i < 27; i++)
-        {
-            if(map1[i] == 0 && map2[i] == 0) continue;
-            if(map1[i] != 0 && map2[i] != 0) continue;// dono mein hai vo character so we can still swap and get the string
-
-            return false;
+        for(char c: word2.toCharArray()){
+            map2[c-'a']++;
         }
 
-        Arrays.sort(map1);
-        Arrays.sort(map2);
-
-        for(int i = 0 ; i < 27; i++)
-        {
-            if(map1[i] != map2[i]) return false;
+        int occurence[] = new int[word2.length() + 1];
+        for(int i = 0 ; i < 26; i++){
+            if(map2[i] != 0 && map1[i] == 0) return false;
         }
+        for(int i = 0; i < 26; i++){
+            if(map1[i] != 0) occurence[map1[i]]++;
+            if(map2[i] != 0) occurence[map2[i]]--;
+        }
+        for(int i: occurence) if(i != 0) return false;
         return true;
-    } 
+    }
 }
