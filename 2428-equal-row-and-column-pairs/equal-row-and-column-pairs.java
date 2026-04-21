38164d323eddc9bf6fa85d91c84cfe5a304c69be
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-22 02:38:11 +0530

    Time: 63 ms (16.94%) | Memory: 50.6 MB (80.65%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,29 +1,26 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int ans = 0;
-        int n = grid.length;
-        int m = grid[0].length; 
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for(int row[]: grid)
-        {
-            String str = "";
-            for(int i: row)
-            {
-                str =str + i +",";
+        HashMap<String, Integer> rowMap = new HashMap<>();
+        HashMap<String, Integer> colMap = new HashMap<>();
+        int n = grid.length, i = 0, j = 0;
+        while(n-- > 0){
+            StringBuilder rowString = new StringBuilder();
+            for(int col = 0; col < grid.length; col++){
+                rowString.append(grid[i][col]+"|");
             }
-            map.put(str, map.getOrDefault(str,0) + 1);
+            StringBuilder colString = new StringBuilder();
+            for(int row = 0; row < grid.length; row++){
+                colString.append(grid[row][j]+"|");
+            }
+            rowMap.put(rowString.toString(), rowMap.getOrDefault(rowString.toString(), 0) + 1);
+            colMap.put(colString.toString(), colMap.getOrDefault(colString.toString(), 0) + 1);
+            i++;
+            j++;
         }
-        for(int i = 0 ; i < m ; i++)
-        {
-            String str ="";
-            for(int j = 0 ; j < n; j++)
-            {
-                str += grid[j][i] +",";
-            }
-            if(map.containsKey(str))
-            {
-                ans += map.get(str);
+        int ans = 0;
+        for(String key: rowMap.keySet()){
+            if(colMap.containsKey(key)){
+                ans += (rowMap.get(key) * colMap.get(key));
             }
         }
         return ans;
