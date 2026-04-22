7770fa29d48831c02b2a37e8fcfd9bc4901f537c
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-23 02:01:02 +0530

    Time: 5 ms (65.62%) | Memory: 46.9 MB (71.95%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,25 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-
-        for(int elm: asteroids){
-            while(!st.isEmpty() && st.peek() > 0 && elm < 0)
-            {
-                int diff= st.peek() - Math.abs(elm);
-                if(diff < 0)
-                {
+        for(int i: asteroids){
+            while(!st.isEmpty() && st.peek() > 0 && i < 0){
+                int diff = st.peek() - Math.abs(i);
+                if(diff < 0){
                     st.pop();
-                }else if(diff > 0) //ignore by making elm = 0 only adding -ve || +ve 
-                {
-                    elm = 0;
+                }else if(diff > 0){
+                    i = 0;
                 }else{
-                    elm = 0;
-                    st.pop(); 
+                    i = 0;
+                    st.pop();
                 }
             }
-            if(elm < 0 || elm > 0)
-            {
-                st.add(elm);
-            }
+            if(i > 0 || i < 0) st.add(i);
         }
-        int ans[]= new int[st.size()];
 
-        for(int i = st.size() - 1; i >= 0; i--)
-        {
-            ans[i] = st.pop();
+        int index = st.size() - 1;
+        int ans[] = new int[st.size()];
+        while(!st.isEmpty()){
+            ans[index--] = st.pop();
         }
         return ans;
     }
