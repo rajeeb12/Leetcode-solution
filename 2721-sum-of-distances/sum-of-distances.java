15ed50ea5f972eba9d4fe70170b7097bf3357b11
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-04-24 01:06:46 +0530

    Time: 31 ms (30.09%) | Memory: 123.5 MB (24.78%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -1,30 +1,26 @@
 class Solution {
     public long[] distance(int[] nums) {
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        int n = nums.length;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
         for(int i = 0; i < nums.length; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], new ArrayList<>());
-            }
+            if(!map.containsKey(nums[i])) map.put(nums[i], new ArrayList<>());
             map.get(nums[i]).add(i);
         }
-        long ans[] = new long[n];
-        for(Map.Entry<Integer, List<Integer>> e: map.entrySet()){
-            int number = e.getKey();
-            List<Integer> indexes = e.getValue();
-            long total = 0;
-            for(int i: indexes) total += i;
 
-            long prefix = 0;
-            for(int i = 0; i < indexes.size(); i++){
-                int index = indexes.get(i);
-                long postSum = total - prefix - index;
+        long ans[] = new long[nums.length];
+        for(List<Integer> ls: map.values()){
+            long totalSum = 0;
+            for(int i: ls) totalSum += i;
 
-                long left = (long) (index * 1L * (i)) - prefix;
-                long right = (long) postSum - (index * 1L * (indexes.size() - i - 1));
-                long cal = (long) left + right;
-                ans[index] = cal;
-                prefix += indexes.get(i);
+            long leftSum = 0;
+
+            for(int i = 0; i < ls.size(); i++){
+                long rightSum = totalSum - leftSum - ls.get(i);
+
+                long left = (long) (ls.get(i) * 1L * i) - leftSum;
+                long right = rightSum - (long) (ls.get(i) * 1L *(ls.size() - i - 1));
+                ans[ls.get(i)] = (long) left + right;
+                leftSum += ls.get(i);
             }
         }
         return ans;
