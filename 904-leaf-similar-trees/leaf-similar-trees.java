be9151ab8d34e538f1b4232ec3ce8e4d78698a05
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-05-05 00:47:26 +0530

    Time: 6 ms (3.26%) | Memory: 43.4 MB (16.79%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -15,33 +15,25 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        if((root1 == null && root2 != null) || (root1 != null && root2 == null)) return false;
-        if(root1 == root2) return true;
+        StringBuilder left = new StringBuilder("");
+        StringBuilder right = new StringBuilder("");
 
-        List<Integer> temp1 = new ArrayList<>();
-        List<Integer> temp2 = new ArrayList<>();
-        getLeafNodeValues(root1, temp1);
-        getLeafNodeValues(root2, temp2);
+        solve(root1, left);
+        solve(root2, right);
+        System.out.print(left+" "+right);
 
-        if(temp1.size() != temp2.size()) return false;
-        
-        for(int i= 0 ; i < temp1.size(); i++)
-        {
-            if(temp1.get(i) != temp2.get(i)) return false;
-        }
-        return true;
-    }
-    public void getLeafNodeValues(TreeNode root,List<Integer> ls)
-    {
-        if(root == null) return;
+        if(left.length() != right.length()) return false;
 
-        if(isLeaf(root)) ls.add(root.val);
-        getLeafNodeValues(root.left, ls);
-        getLeafNodeValues(root.right, ls);
-    }
-    public boolean isLeaf(TreeNode root)
-    {
-        if(root.left == null && root.right == null) return true;
+        if(left.toString().equals(right.toString())) return true;
         return false;
+    }
+    public void solve(TreeNode node,StringBuilder str){
+        if(node == null) return;
+        if(node.left == null && node.right == null){
+            str.append(node.val+"|");
+            return;
+        }
+        solve(node.left, str);
+        solve(node.right, str);
     }
 }
