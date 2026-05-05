e4e0a859afb6a9600f7804967c3b3ffdb7f8109d
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-05-06 01:25:41 +0530

    Time: 2 ms (94.28%) | Memory: 57.1 MB (28.53%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -14,20 +14,22 @@
  * }
  */
 class Solution {
+    int count = 0;
     public int goodNodes(TreeNode root) {
-        if(root.left == null && root.right == null) return root.val;
-        int count[] = new int[1];
-    
-        solve(root,count,Integer.MIN_VALUE);
-        return count[0];
+        if(root == null) return 0;
+        solve(root, root.val);
+        return count;
     }
-    public void solve(TreeNode root,int[] count,int prev_max)
-    {
+    public void solve(TreeNode root, int max){
         if(root == null) return;
 
-        if(root.val >= prev_max) count[0]++;
+        if(root.val >= max){
+            count++;
+        }
 
-        solve(root.left,count, Math.max(root.val, prev_max));
-        solve(root.right, count, Math.max(root.val, prev_max));
+        solve(root.left, Math.max(max, root.val));
+        solve(root.right, Math.max(max, root.val));
+
     }
+
 }
