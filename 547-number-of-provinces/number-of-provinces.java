1f58f139b33630965a73aeaa41a8316cc4b21496
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-05-27 01:06:39 +0530

    Time: 3 ms (27.71%) | Memory: 49.8 MB (5.42%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,45 +1,30 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        for(int i = 0 ; i < n; i++)
-        {
+        for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0; j < n ; j++)
-            {
-                if(i == j) continue;
-                if(isConnected[i][j] != 0)
-                {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j] != 0) adj.get(i).add(j);
             }
         }
-        int visit[] = new int[n];
-
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(visit[i] == 0)
-            {
-                count++;
-                dfs(i,adj,visit);
-            }
+        int count = 0;
+        boolean visited[] = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(visited[i]) continue;
+            count++;
+            helper(i,adj,visited);
         }
         return count;
     }
-    public void dfs(int node, ArrayList<ArrayList<Integer>> adj , int[] visit)
-    {
-        visit[node] = 1;
-
-        for(int adjNode: adj.get(node))
-        {
-            if(visit[adjNode] == 0)
-            {
-                dfs(adjNode,adj,visit);
+    public void helper(int node,List<List<Integer>> adj,boolean visited[]){
+        visited[node] = true;
+        for(int i = 0; i < adj.get(node).size(); i++){
+            int adjNode = adj.get(node).get(i);
+            if(!visited[adjNode]){
+                helper(adjNode, adj, visited);
             }
         }
     }
