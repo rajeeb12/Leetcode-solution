48902e7c9427f29ce26ff5e9dafab79f06975a1b
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-06-11 18:04:02 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (71.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,19 +1,20 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
+        int m = grid[0].length, fresh = 0;
         Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2) q.add(new int[]{i, j});
+                if(grid[i][j] == 1) fresh++;
             }
         }
 
         int di[] = {1,-1,0,0};
         int dj[] = {0,0,1,-1};
-        int time = 0, flag = 1;
-        while(!q.isEmpty()){
+        int time = 0;
+        while(!q.isEmpty() && fresh > 0){
             int size = q.size();
 
             for(int i = 0; i < size; i++){
@@ -26,21 +27,14 @@ class Solution {
                     if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 1){
                         q.add(new int[]{nrow, ncol});
                         grid[nrow][ncol] = 2;
+                        fresh--;
                     }
                 }
-            }
-            if(flag == 1){
-                flag = 0;
-                continue;            
             }
             time++;
            
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1) return -1;
-            }
-        }
-        return time;
+        if(fresh == 0) return time;
+        return -1;
     }
 }
