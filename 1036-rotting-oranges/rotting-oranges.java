0340f6d728c700cae94514d23cb707d767e1c7a6
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-06-11 18:00:10 +0530

    Time: 2 ms (90.77%) | Memory: 44.3 MB (40.11%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,75 +1,46 @@
-class Pair{
-    int row;
-    int col;
-    public Pair(int _r,int _c)
-    {
-        this.row = _r;
-        this.col = _c;
-    }
-}
 class Solution {
-    int n;
-    int m;
     public int orangesRotting(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
 
-        Queue<Pair> q = new LinkedList<>();
-        int visited[][] = new int[n][m];
-
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < m; j++)
-            {
-                if(grid[i][j] == 2)
-                {
-                    q.add(new Pair(i,j));
-                    visited[i][j] = 1;
-                }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 2) q.add(new int[]{i, j});
             }
         }
-        int di[]= {1,-1,0,0};
-        int dj[]= {0,0,1,-1};
-        int time = 0;
-        while(!q.isEmpty())
-        {
+
+        int di[] = {1,-1,0,0};
+        int dj[] = {0,0,1,-1};
+        int time = 0, flag = 1;
+        while(!q.isEmpty()){
             int size = q.size();
 
-            for(int i = 0; i < size; i++)
-            {
-                Pair p = q.poll();
-                int r = p.row;
-                int c = p.col;
-
-                for(int k = 0; k < 4; k++)
-                {
-                    int row = r + di[k];
-                    int col = c + dj[k];
-                    if(check(row, col) && grid[row][col] == 1 && visited[row][col] == 0)
-                    {
-                        grid[row][col] = 2;
-                        visited[row][col] = 1;
-                        q.add(new Pair(row, col));
+            for(int i = 0; i < size; i++){
+                int temp[] = q.poll();
+                int row = temp[0];
+                int col = temp[1];
+                for(int k = 0; k < 4; k++){
+                    int nrow = row + di[k];
+                    int ncol = col + dj[k];
+                    if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 1){
+                        q.add(new int[]{nrow, ncol});
+                        grid[nrow][ncol] = 2;
                     }
                 }
             }
+            if(flag == 1){
+                flag = 0;
+                continue;            
+            }
             time++;
+           
         }
-        for(int i = 0 ; i < n; i++)
-        {
-            for(int j = 0 ; j < m; j++)
-            {
-                if(grid[i][j] == 1)
-                {
-                    return -1;
-                }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1) return -1;
             }
         }
-        return (time == 0 ? 0 : time - 1);
-    }
-    public boolean check(int i,int j)
-    {
-        if(i < 0 || j < 0 || i >= n || j >= m) return false;
-        return true;
+        return time;
     }
 }
