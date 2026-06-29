2d0d9dddbdb9287dab9df44466cc430e482b4b9f
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-06-30 01:55:15 +0530

    Time: 20 ms (78.33%) | Memory: 53.9 MB (68.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,10 +4,20 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         dp = new int[n + 1][m + 1];
-        
-        for(int r[]: dp) Arrays.fill(r, -1);
 
-        return solve(0,0,text1, text2);
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                int ans = 0;
+                if(text1.charAt(i) == text2.charAt(j)){
+                    ans = 1 + dp[i + 1][j + 1];
+                }else{
+                    ans = Math.max(dp[i + 1][j] , dp[i][j + 1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
     }
     public int solve(int i,int j,String t1, String t2){
         if(i >= t1.length() || j >= t2.length()) return 0;
