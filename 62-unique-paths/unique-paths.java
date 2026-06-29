070499ef80cf033f377d7dcba675d35c54be7eeb
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-06-30 01:10:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (47.22%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,40 +1,26 @@
 class Solution {
+    int di[] = {0,1};
+    int dj[] = {1,0};
     int dp[][];
     public int uniquePaths(int m, int n) {
         dp = new int[m + 1][n + 1];
-        
+        for(int r[]: dp) Arrays.fill(r, -1);
 
-        for(int row = 0 ; row < m; row++)
-        {
-            for(int col = 0 ; col < n; col++)
-            {
-                if(row == 0 && col == 0)
-                {
-                    dp[row][col] = 1;
-                    continue;
-                }
-                int sum = 0;
-                 
-                if(row > 0) sum += dp[row - 1][col];
-                if(col > 0) sum += dp[row][col - 1];
+        return solve(0,0,m - 1, n - 1);
+    }
+    public int solve(int i,int j,int m,int n){
+        if(i == m && j == n) return 1;
 
-                dp[row][col] = sum;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int ans = 0;
+        for(int k = 0; k < 2; k++){
+            int ni = i + di[k];
+            int nj = j + dj[k];
+            if(ni <= m && nj <= n){
+                ans += solve(ni, nj, m, n);
             }
         }
-        return dp[m - 1][n - 1];
-        //return solve(m - 1, n - 1);
-    }
-    public int solve(int row, int col)
-    {
-        if(row < 0 || col < 0) return 0;
-        if(row == 0 && col == 0) return 1;
-
-        if(dp[row][col] != -1) return dp[row][col];
-        int sum = 0;
-
-        sum += solve(row - 1, col);
-        sum += solve(row, col - 1);
-
-        return dp[row][col] = sum;
+        return dp[i][j] = ans;
     }
 }
