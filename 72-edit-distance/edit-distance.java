ed01bb925a30af6893f0c30b7d37eb67f2c05e91
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-07-05 22:45:21 +0530

    Time: 3 ms (98.30%) | Memory: 47.4 MB (6.18%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,56 +1,30 @@
 class Solution {
     int dp[][];
-    public int minDistance(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        dp = new int[n + 1][m + 1];
-        int prev[] = new int[m + 1];
+    public int minDistance(String word1, String word2) {
+        if(word1.equals(word2)) return 0;
 
-        // for(int i = 0 ; i <= n; i++)
-        // {
-        //     dp[i][0] = i;
-        // }
-        // for(int j = 0; j <= m; j++)
-        // {
-        //     dp[0][j] = j;
-        // }
-        for(int j = 0 ; j <= m; j++)
-        {
-            prev[j] = j; 
-        }
-        for(int i = 1; i <=n; i++)
-        {
-            int cur[] = new int[m + 1];
-            cur[0] = i;
-            for(int j = 1; j <= m; j++)
-            {
-                if(s1.charAt(i - 1) == s2.charAt(j - 1))
-                {
-                    cur[j]  = prev[j -1];
-                }
-                else{
-                    cur[j] = (Math.min(1 + cur[j - 1] , Math.min( 1 + prev[j - 1], 1 + prev[j])));
-                }
-            }
-            prev = cur;
-        }
-        return prev[m] ;
-        //return solve(word1, word2, n-1, m-1);
+        int m = word1.length();
+        int n = word2.length();
+        dp= new int[m + 1][n + 1];
+        for(int r[]: dp) Arrays.fill(r, -1);
+
+        return solve(m - 1, n - 1, word1, word2);
     }
-    public int solve(String s1, String s2, int i,int j)
-    {
-        if(i < 0)
-        {
-            return j + 1;
-        }
+    public int solve(int i,int j,String s1, String s2){
+        if(i < 0) return j + 1;
         if(j < 0) return i + 1;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1.charAt(i) == s2.charAt(j))
-        {
-            return 0 + solve(s1, s2, i - 1, j - 1);
+        int ans = 0;
+        if(s1.charAt(i) == s2.charAt(j)){
+            ans = solve(i - 1, j - 1, s1, s2);
+        }else{
+            int insert = 1 + solve(i, j - 1, s1, s2);
+            int replace = 1 + solve(i - 1, j - 1, s1, s2);
+            int delete = 1 + solve(i - 1, j, s1, s2);
+            ans = Math.min(insert, Math.min(replace, delete)); 
         }
-        return dp[i][j] = ( Math.min(1 + solve(s1, s2, i, j - 1) , Math.min( 1 + solve(s1, s2, i - 1, j - 1), 1 + solve(s1, s2, i - 1, j))));
+        return dp[i][j] = ans;
     }
 }
