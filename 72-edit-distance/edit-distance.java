0351f1762d970415c044cdbc8ca0f27bd4b41670
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-07-05 22:59:02 +0530

    Time: 5 ms (69.24%) | Memory: 46.9 MB (88.91%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -6,9 +6,25 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         dp= new int[m + 1][n + 1];
-        for(int r[]: dp) Arrays.fill(r, -1);
+        for(int i = 0; i <= n; i++) dp[0][i] = i;
+        for(int j = 0; j <= m; j++) dp[j][0] = j;
 
-        return solve(m - 1, n - 1, word1, word2);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                int ans = 0; 
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    ans = dp[i - 1][j - 1];
+                }else{
+                    int insert = dp[i][j -1];
+                    int replace = dp[i - 1][j - 1];
+                    int delete = dp[i - 1][j];
+                    ans = 1 + Math.min(insert, Math.min(replace, delete)); 
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[m ][n ];
     }
     public int solve(int i,int j,String s1, String s2){
         if(i < 0) return j + 1;
