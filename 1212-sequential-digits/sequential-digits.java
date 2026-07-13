33e68cd33063cf08277824fde81ce9c5b81a95d3
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2026-07-14 03:18:53 +0530

    Time: 1 ms (28.11%) | Memory: 42.6 MB (6.43%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i <= 8; i++)
-        {
+        for(int i = 1; i <= 8; i++){
             q.add(i);
         }
-
         List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty())
-        {
-            int num = q.poll();
-            System.out.print(num+" ");
-            if(num >= low && num <= high)
-            {
-                ans.add(num);
-            }   
-            
-            int ld = num % 10;    
-            if(ld + 1 <= 9 ) 
-            {
-                num = num * 10 + (ld + 1);
-                q.add(num);
+        while(!q.isEmpty()){
+            int num = q.peek();
+            if(num >= high){
+                break;
             }
-            
+            int size = q.size();
+            for(int i = 1; i <= size; i++){
+                int curNum = q.poll();
+                int lastDigit = curNum % 10;
+                int nextNum = (curNum * 10)  + (lastDigit + 1);
+                if(lastDigit >= 9) break;
+                
+                q.add(nextNum);
+                if(nextNum >= low && nextNum <= high){
+                    ans.add(nextNum);
+                }
+            }
         }
+        
         return ans;
     }
 }
